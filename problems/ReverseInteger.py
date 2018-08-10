e413ea5f264a0dd0171cb982663e1906ad73051f
wanwan <rarry2012@gmail.com>
2018-08-10 12:43:36 -0400

    add a new reverse with much better performance

--- problems/ReverseInteger.py
+++ problems/ReverseInteger.py
@@ -1,5 +1,5 @@
 class Solution:
-    def reverse(self, x):
+    def reverse2(self, x):
         """
         :type x: int
         :rtype: int
@@ -13,5 +13,23 @@ class Solution:
         if revsedIntX < -2 ** 31 or revsedIntX > 2 ** 31 -1 :
             revsedIntX = 0
         return revsedIntX
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        flag = 1 if x > -1 else -1
+        positiveX = flag * x
+        res = 0
+        while positiveX > 0:
+            res = res * 10 + positiveX % 10
+            positiveX = positiveX // 10
+
+        res = res * flag
+        if res < -2 ** 31 or res > 2 ** 31 -1 :
+            res = 0
+
+        return  res
 
 
