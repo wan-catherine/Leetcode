f03cedae76fd0e092a8e10e2a2c27322ea2dfe1f
wanwan <rarry2012@gmail.com>
2018-10-13 11:28:37 -0400

    90	 Subsets II
    backtracking

--- problems/SubsetsII.py
+++ problems/SubsetsII.py
@@ -1,6 +1,28 @@
 class Solution:
+    # backtracking
     def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        res = []
+        if nums == None or len(nums) < 1:
+            return res.append([])
+
+        nums = sorted(nums)
+        subset = []
+        self.toFindAllSubsets(nums, res, subset, 0)
+        return res
+
+    def toFindAllSubsets(self, nums, res, subset, index):
+        res.append(subset.copy())
+
+        for i in range(index, len(nums)):
+            if i != index and nums[i - 1] == nums[i]:
+                continue
+
+            subset.append(nums[i])
+            self.toFindAllSubsets(nums, res, subset, i+1)
+            subset.pop()
+
+
--- test/test_subsets_ii.py
+++ test/test_subsets_ii.py
@@ -1,6 +1,16 @@
 from unittest import TestCase
-
+from problems.SubsetsII import Solution
 
 class TestSolution(TestCase):
     def test_subsetsWithDup(self):
-        self.fail()
+        solution = Solution()
+        res = solution.subsetsWithDup([1,2,2])
+        output = [
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+        self.assertEqual(sorted(output), sorted(res))
