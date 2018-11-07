1d92370fba7bd21a46ddf06194f03317c331a2eb
wanwan <rarry2012@gmail.com>
2018-11-06 19:31:44 -0500

    122	 Best Time to Buy and Sell Stock II

--- problems/BestTimeToBuySellStockII.py
+++ problems/BestTimeToBuySellStockII.py
@@ -4,3 +4,31 @@ class Solution:
         :type prices: List[int]
         :rtype: int
         """
+        if prices == None or len(prices) <= 1:
+            return 0
+
+        res = 0
+        i = 1
+        buy = None
+        while i < len(prices):
+            if prices[i-1] > prices[i]:
+                if buy != None and prices[i-1] > buy:
+                    res += prices[i-1] - buy
+                    buy = None
+            else:
+                if buy == None and prices[i-1] < prices[i]:
+                    buy = prices[i-1]
+                if i == len(prices) - 1 and buy != None and buy < prices[-1]:
+                    res += prices[i] - buy
+            i += 1
+        return res
+
+    def maxProfit_better(self, prices):
+        max_profit = 0
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1]:
+                max_profit += prices[i + 1] - prices[i] 
+
+        return max_profit
+
+
--- test/test_best_time_buy_sell_stock_ii.py
+++ test/test_best_time_buy_sell_stock_ii.py
@@ -1,6 +1,28 @@
 from unittest import TestCase
-
+from problems.BestTimeToBuySellStockII import Solution
 
 class TestSolution(TestCase):
-    def test_max_profit_by_empty(self):
-        self.fail()
+    def test_maxProfit(self):
+        solution = Solution()
+        res = solution.maxProfit([7,1,5,3,6,4])
+        self.assertEqual(7, res)
+
+    def test_maxProfit_one(self):
+        solution = Solution()
+        res = solution.maxProfit([1,2,3,4,5])
+        self.assertEqual(4, res)
+
+    def test_maxProfit_two(self):
+        solution = Solution()
+        res = solution.maxProfit([7,6,4,3,1])
+        self.assertEqual(0, res)
+
+    def test_maxProfit_three(self):
+        solution = Solution()
+        res = solution.maxProfit([8,6,4,3,3,2,3,5,8,3,8,2,6])
+        self.assertEqual(15, res)
+
+    def test_maxProfit_four(self):
+        solution = Solution()
+        res = solution.maxProfit([1,9,6,9,1,7,1,1,5,9,9,9])
+        self.assertEqual(25, res)
