8e317f2c74074a136e3625c961ab7b5f9bcd6be9
wanwan <rarry2012@gmail.com>
2018-11-17 12:08:05 -0500

    123	 Best Time to Buy and Sell Stock III

---
+++ problems/BestTimeBuySellStockIII.py
@@ -0,0 +1,52 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        """
+        DP Formular:
+        i: Transaction
+        j: the jth days
+        T[i][j] = max(A, B)
+        
+        A = T[i][j-1]     we don't do any transact at day j 
+        B = T[i-1][m] + prices[j] - prices[m]   we sell at day j , and buy at m , m in (0...j-1)
+        
+        
+        also , we can change B's order :
+        B = prices[j] + (T[i-1][m] - prices[m]
+        
+        Then we can use a variable maxDiff:
+        B = prices[j] + maxDiff
+        maxDiff = max(maxDiff, T[i-1][j-1] - prices[j-1])
+        """
+        # if prices == None or len(prices) == 0:
+        #     return 0
+        #
+        # res = [[0 for i in range(len(prices))] for j in range(3)]
+        # for i in range(1,3):
+        #     maxDiff = res[i][0] - prices[0]
+        #     for j in range(1, len(prices)):
+        #         maxDiff = max(maxDiff, res[i-1][j-1] - prices[j-1])
+        #         res[i][j] = max(res[i][j-1], maxDiff+prices[j])
+        # return res[-1][-1]
+        return self.maxProfitN(prices, 2)
+
+    def maxProfitN(self, prices, n):
+        """
+        :type prices: List[int]
+        :type n: the most tranaction times
+        :rtype: int
+        """
+        if prices == None or len(prices) == 0:
+            return 0
+
+        res = [[0 for i in range(len(prices))] for j in range(n+1)]
+        for i in range(1,n+1):
+            maxDiff = res[i][0] - prices[0]
+            for j in range(1, len(prices)):
+                maxDiff = max(maxDiff, res[i-1][j-1] - prices[j-1])
+                res[i][j] = max(res[i][j-1], maxDiff+prices[j])
+        return res[-1][-1]
---
+++ test/test_best_time_buy_sell_stock_iii.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from problems.BestTimeBuySellStockIII import Solution
+
+class TestSolution(TestCase):
+    def test_maxProfit(self):
+        solution = Solution()
+        res = solution.maxProfit([3,3,5,0,0,3,1,4])
+        self.assertEqual(6, res)
+
+    def test_maxProfit_one(self):
+        solution = Solution()
+        res = solution.maxProfit([1,2,3,4,5])
+        self.assertEqual(4, res)
+
+    def test_maxProfit_two(self):
+        solution = Solution()
+        res = solution.maxProfit([7,6,4,3,1])
+        self.assertEqual(0, res)
+
+    def test_maxProfit_three(self):
+        solution = Solution()
+        res = solution.maxProfit([2,5,7,1,4,3,1,3])
+        self.assertEqual(8, res)
