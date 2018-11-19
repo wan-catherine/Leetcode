67c8dd02f03a01d7c6eba9db9e44cf6795ce8b65
wanwan <rarry2012@gmail.com>
2018-11-19 18:03:45 -0500

    154	 Find Minimum in Rotated Sorted Array II

---
+++ problems/FindMinimumRotatedSortedArrayII.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return nums[0]
+
+        if nums[0] < nums[-1]:
+            return nums[0]
+
+        i = 1
+        while i < len(nums):
+            if nums[i] < nums[i - 1]:
+                return nums[i]
+            i += 1
+        return nums[-1]
---
+++ test/test_Find_minimum_Rotated_Sorted_Array_II.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from problems.FindMinimumRotatedSortedArrayII import Solution
+
+class TestSolution(TestCase):
+    def test_findMin(self):
+        solution = Solution()
+        res = solution.findMin([1,3,5])
+        self.assertEqual(1, res)
+
+    def test_findMin_one(self):
+        solution = Solution()
+        res = solution.findMin([2,2,2,0,1])
+        self.assertEqual(0, res)
+
+    def test_findMin_two(self):
+        solution = Solution()
+        res = solution.findMin([2,2,2,2,2])
+        self.assertEqual(2, res)
+
+    def test_findMin_three(self):
+        solution = Solution()
+        res = solution.findMin([1])
+        self.assertEqual(1, res)
+
+    def test_findMin_four(self):
+        solution = Solution()
+        res = solution.findMin([3,3,3,3,3,3,3,3,1,3])
+        self.assertEqual(1, res)
