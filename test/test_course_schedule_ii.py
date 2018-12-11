541ca1dd5a42ab6f1aee3ed15dbd4fec22ad8a0f
wanwan <rarry2012@gmail.com>
2018-12-11 18:38:50 -0500

    210	 Course Schedule II

---
+++ test/test_course_schedule_ii.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from problems.CourseScheduleII import Solution
+
+class TestSolution(TestCase):
+    def test_findOrder(self):
+        solution = Solution()
+        res = solution.findOrder(2, [[1,0]] )
+        self.assertEqual(res, [0,1])
+
+    def test_findOrder_one(self):
+        solution = Solution()
+        res = solution.findOrder(4, [[1,0],[2,0],[3,1],[3,2]])
+        self.assertEqual(res, [0,1,2,3] )
