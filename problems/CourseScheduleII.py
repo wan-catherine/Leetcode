851916c22d9e9ed810785a773e3f8585e5e9ba5a
wanwan <rarry2012@gmail.com>
2018-12-12 11:04:22 -0500

    210	 Course Schedule II

--- problems/CourseScheduleII.py
+++ problems/CourseScheduleII.py
@@ -5,3 +5,40 @@ class Solution:
         :type prerequisites: List[List[int]]
         :rtype: List[int]
         """
+        if numCourses < 1:
+            return []
+
+        count = [0]*numCourses
+        dic = {}
+        for l in prerequisites:
+            count[l[0]] += 1
+            if l[1] not in dic:
+                dic[l[1]] = [l[0]]
+            else:
+                dic[l[1]].append(l[0])
+
+        res = [i for i in range(len(count)) if count[i] == 0]
+        if len(res) < 1:
+            return []
+        temp = res.copy()
+
+        while len(res) < numCourses:
+            if len(temp) < 1:
+                break
+            i = temp.pop()
+            if i not in dic:
+                continue
+            for j in dic[i]:
+                count[j] -= 1
+                if count[j] == 0:
+                    res.append(j)
+                    temp.append(j)
+
+        if len(res) == numCourses:
+            return res
+        else:
+            return []
+
+
+
+
--- test/test_course_schedule_ii.py
+++ test/test_course_schedule_ii.py
@@ -11,3 +11,8 @@ class TestSolution(TestCase):
         solution = Solution()
         res = solution.findOrder(4, [[1,0],[2,0],[3,1],[3,2]])
         self.assertEqual(res, [0,1,2,3] )
+
+    def test_findOrder_two(self):
+        solution = Solution()
+        res = solution.findOrder(2, [[0,1],[1,0]] )
+        self.assertEqual(res, [])
