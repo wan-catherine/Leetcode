660c376f1ca38b8a9f2bab8cef57f8c4a4836cf9
wanwan <rarry2012@gmail.com>
2020-04-26 10:19:42 -0400

    Binary search tree

--- problems/ValidateBinarySearchTree.py
+++ problems/ValidateBinarySearchTree.py
@@ -1,4 +1,7 @@
 # Definition for a binary tree node.
+from math import inf
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -6,6 +9,7 @@ class TreeNode:
         self.right = None
 
 class Solution:
+    #pre-order traversal tree and check the val is ordered by ascend
     def isValidBST(self, root):
         """
         :type root: TreeNode
@@ -20,7 +24,6 @@ class Solution:
             node = stack[-1]
             if node.left!= None:
                 stack.append(node.left)
-                node = node.left
                 continue
             if len(values)>0 and node.val <= values[-1]:
                 return False
@@ -31,9 +34,47 @@ class Solution:
                 stack.append(node)
                 continue
 
+            #remove the compared subtrees to avoid duplicating
             if len(stack) > 0:
                 if node == stack[-1].right:
                     stack[-1].right = None
                 else:
                     stack[-1].left = None
         return True
+
+    # pre-order traversal tree
+    def isValidBST_20200426(self, root):
+        if not root:
+            return True
+        stack = []
+        value = float(-inf)
+        current = root
+        while True:
+            if current:
+                stack.append(current)
+                current = current.left
+            elif stack:
+                node = stack.pop()
+                if value >= node.val:
+                    return False
+                    value = node.val
+                current = node.right
+            else:
+                break
+        return True
+
+    #use the up and down limit to check
+    def isValidBST_with_border(self, root):
+        if not root:
+            return True
+        return self.isBST(root, float(-inf), float(inf))
+
+    def isBST(self, root, min, max):
+        if not root:
+            return True
+        if root.val >= max or root.val <= min:
+            return False
+        return self.isBST(root.left, min, root.val) and self.isBST(root.right, root.val, max)
+
+
+
--- test/test_validate_binary_search_tree.py
+++ test/test_validate_binary_search_tree.py
@@ -7,12 +7,12 @@ class TestSolution(TestCase):
         root = TreeNode(2)
         root.right = TreeNode(3)
         root.left = TreeNode(1)
-        res = solution.isValidBST(root)
+        res = solution.isValidBST_20200426(root)
         self.assertEqual(True, res)
 
     def test_isValidBST_one(self):
         solution = Solution()
         root = TreeNode(1)
         root.right = TreeNode(1)
-        res = solution.isValidBST(root)
+        res = solution.isValidBST_20200426(root)
         self.assertEqual(False, res)
