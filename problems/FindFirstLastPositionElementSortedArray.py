979771be236cb13c0db381b3b4e22429b7aa87ea
wanwan <rarry2012@gmail.com>
2020-05-09 18:19:29 -0400

    34

--- problems/FindFirstLastPositionElementSortedArray.py
+++ problems/FindFirstLastPositionElementSortedArray.py
@@ -45,7 +45,7 @@ class Solution:
             return [-1, -1]
         return [i, j]
 
-    def searchRange(self, nums, target):
+    def searchRange_before(self, nums, target):
         i = 0
         j = len(nums) - 1
         if j < i:
@@ -69,3 +69,28 @@ class Solution:
             else:
                 i = middle
         return [left, j]
+
+    # binary search
+    def searchRange(self, nums, target):
+        if not nums or len(nums) < 1:
+            return [-1, -1]
+        i = 0
+        j = len(nums)
+        while i < j:
+            mid = i + (j - i) // 2
+            if nums[mid] >= target:
+                j = mid
+            elif nums[mid] < target:
+                i = mid + 1
+
+        left = j if j < len(nums) and nums[j] == target else -1
+        i = 0
+        j = len(nums)
+        while i < j:
+            mid = i + (j - i) // 2
+            if nums[mid] <= target:
+                i = mid + 1
+            elif nums[mid] > target:
+                j = mid
+        right = i - 1 if nums[i-1] == target else -1
+        return [left, right]
