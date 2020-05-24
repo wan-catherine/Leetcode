e7e990a48a28163612f24c6ab17faa472c5d3e61
wanwan <rarry2012@gmail.com>
2020-05-24 16:23:14 -0400

    update

--- problems/N932_Beautiful_Array.py
+++ problems/N932_Beautiful_Array.py
@@ -9,3 +9,10 @@ class Solution(object):
         while len(res) < N:
             res = [i * 2 -1 for i in res] + [i*2 for i in res]
         return [i for i in res if i <= N]
+
+    def beautifulArray_recursive(self, N):
+        if N == 1:
+            return [1]
+        odd = [i * 2 -1 for i in self.beautifulArray((N+1)//2)]
+        even = [i * 2 for i in self.beautifulArray(N//2)]
+        return odd + even
