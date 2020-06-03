4aa61ec1b9b960e8319afb5f416d0288cb9dcdb0
wanwan <rarry2012@gmail.com>
2020-06-03 19:07:14 -0400

    Update N402_Remove_K_Digits.py

--- problems/N402_Remove_K_Digits.py
+++ problems/N402_Remove_K_Digits.py
@@ -6,7 +6,7 @@ if loop out the num and k > 0 , the we know all the left digits are same .
 so just remove the last k digits , we will get the result.
 """
 class Solution:
-    def removeKdigits(self, num: str, k: int) -> str:
+    def removeKdigits_before(self, num: str, k: int) -> str:
         digits = []
         i = 0
         while i < len(num):
@@ -26,3 +26,16 @@ class Solution:
         digits=digits[:-k]  if k  else digits
         res = ''.join(digits).lstrip('0')
         return res if res else "0"
+
+    # non-decreasing monotonic stack
+    def removeKdigits(self, num, k):
+        stack = []
+        for i in num:
+            while stack and k and int(stack[-1]) > int(i):
+                stack.pop()
+                k -= 1
+            stack.append(i)
+        # in case k > 0
+        stack = stack[:-k] if k else stack
+        res = ''.join(stack).lstrip('0')
+        return res if res else '0'
