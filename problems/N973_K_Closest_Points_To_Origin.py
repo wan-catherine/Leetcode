426b6d202c384e5d96b4a07f2da7558ae95eee6f
wanwan <rarry2012@gmail.com>
2020-06-20 21:42:36 -0400

    973

--- problems/N973_K_Closest_Points_To_Origin.py
+++ problems/N973_K_Closest_Points_To_Origin.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class Solution(object):
     def kClosest_my_solution(self, points, K):
         """
@@ -28,3 +31,30 @@ class Solution(object):
     def kClosest_other_fast_solution(self, points, K):
         points.sort(key = lambda x : x[0] ** 2 + x[1] ** 2)
         return points[:K]
+
+    # bucket sort
+    # because point[0]**2 + point[1]**2 maybe very large.
+    # the range [min_distance, max_distance] maybe very large
+    def kClosest_timeout(self, points, K):
+        if not points:
+            return []
+        distance_points_mapping = collections.defaultdict(list)
+        min_distance = points[0][0] ** 2 + points[0][1] ** 2
+        max_distance = 0
+        for point in points:
+            key = point[0] ** 2 + point[1] ** 2
+            min_distance = min(min_distance,key)
+            max_distance = max(max_distance, key)
+            distance_points_mapping[key].append(point)
+        res = []
+        while min_distance <= max_distance:
+            length = len(distance_points_mapping[min_distance])
+            if K >= length:
+                K -= length
+                res.extend(distance_points_mapping[min_distance])
+                min_distance += 1
+            else:
+                res.extend(distance_points_mapping[min_distance][:K])
+                break
+        return res
+
--- test/test_973_k_closest_points_to_origin.py
+++ test/test_973_k_closest_points_to_origin.py
@@ -7,3 +7,6 @@ class TestSolution(TestCase):
 
     def test_kClosest_1(self):
         self.assertEqual([[3,3],[-2,4]], Solution().kClosest([[3,3],[5,-1],[-2,4]], 2))
+
+    def test_kClosest_2(self):
+        self.assertEqual([[0,1],[1,0]], Solution().kClosest([[0,1],[1,0]], 2))
