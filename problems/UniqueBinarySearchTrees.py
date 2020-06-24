aaece5b7775a993a93dc3605324fde588c9fca85
wanwan <rarry2012@gmail.com>
2020-06-24 09:59:55 -0400

    96

--- problems/UniqueBinarySearchTrees.py
+++ problems/UniqueBinarySearchTrees.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numTrees(self, n):
+    def numTrees_before(self, n):
         """
         :type n: int
         :rtype: int
@@ -16,4 +16,12 @@ class Solution:
                 count += res[j-1]*res[i-j]
             res.append(count)
             i += 1
+        return res[n]
+
+    def numTrees(self, n):
+        res = [0]*(n+1)
+        res[0] = 1
+        for i in range(1,n+1):
+            for j in range(1,i+1):
+                res[i] += res[i-j] * res[j-1]
         return res[n]
