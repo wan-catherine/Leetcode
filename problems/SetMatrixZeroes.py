9f19fb079f516729fcda8909743872d5bdea7c8b
wanwan <rarry2012@gmail.com>
2020-06-29 15:43:40 -0400

    73

--- problems/SetMatrixZeroes.py
+++ problems/SetMatrixZeroes.py
@@ -1,5 +1,5 @@
 class Solution:
-    def setZeroes(self, matrix):
+    def setZeroes_before(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
@@ -34,6 +34,54 @@ class Solution:
             if lc[i] == 0:
                 for c in range(rows):
                     matrix[c][i] = 0
+
+    """
+    use first row/col to same the zero information. 
+    Before that, need to check if the first row/col themselves need to change 
+    """
+    def setZeroes(self, matrix):
+        if not matrix or not matrix[0]:
+            return
+
+        col_zero = False
+        row_zero = False
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for row in range(rows):
+            if matrix[row][0] == 0:
+                col_zero = True
+                break
+
+        for col in range(cols):
+            if matrix[0][col] == 0:
+                row_zero = True
+                break
+
+        for row in range(1, rows):
+            for col in range(1, cols):
+                if matrix[row][col] == 0:
+                    matrix[row][0], matrix[0][col] = 0, 0
+
+        for row in range(1,rows):
+            if matrix[row][0] == 0:
+                for c in range(cols):
+                    matrix[row][c] = 0
+
+        for col in range(1,cols):
+            if matrix[0][col] == 0:
+                for r in range(rows):
+                    matrix[r][col] = 0
+
+        if row_zero:
+            for col in range(cols):
+                matrix[0][col] = 0
+        if col_zero:
+            for row in range(rows):
+                matrix[row][0] = 0
+
+        return matrix
 
 
 
--- test/test_set_matrix_zeroes.py
+++ test/test_set_matrix_zeroes.py
@@ -43,3 +43,9 @@ class TestSolution(TestCase):
         input = [[1,0,3]]
         solution.setZeroes(input)
         self.assertEqual([[0, 0, 0]], input)
+
+    def test_setZeroes_1(self):
+        solution = Solution()
+        input = [[0,0,0,5],[4,3,1,4],[0,1,1,4],[1,2,1,3],[0,0,1,1]]
+        solution.setZeroes(input)
+        self.assertEqual([[0, 0, 0, 0], [0, 0, 0, 4], [0, 0, 0, 0], [0, 0, 0, 3], [0, 0, 0, 0]], input)
