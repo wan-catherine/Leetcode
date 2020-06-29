2676dc76abb6220a2a784980b240d75a6e189fe4
wanwan <rarry2012@gmail.com>
2020-06-28 20:27:15 -0400

    31

--- problems/NextPermutation.py
+++ problems/NextPermutation.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def nextPermutation(self, nums):
+    def nextPermutation_before(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
@@ -25,3 +25,36 @@ class Solution(object):
         if k == -1:
             nums.reverse()
         return nums
+    """
+    find the rules of how to create permutation
+    a. Find the descrease part of the array which means it uses up all possibility
+    b. Change the next number of this descrease arary with the number > itself.
+    c. reverse all the left numbers in the array
+    
+    The key point here is to deal with the corner condition
+    """
+    def nextPermutation(self, nums):
+        length = len(nums)
+        i = length - 1
+        while i > 0:
+            if nums[i] <= nums[i-1]:
+                i -= 1
+            else:
+                break
+        if i < length - 1:
+            if i > 0:
+                for j in range(length-1,-1,-1):
+                    if nums[i-1] < nums[j]:
+                        nums[i-1],nums[j] = nums[j],nums[i-1]
+                        break
+
+            left, right = i, length - 1
+            while left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        else:
+            nums[i],nums[i-1] = nums[i-1], nums[i]
+        return nums
+
+
--- test/test_next_permutation.py
+++ test/test_next_permutation.py
@@ -6,3 +6,18 @@ class TestSolution(TestCase):
         solution = Solution()
         res = solution.nextPermutation([1,2,3])
         self.assertEqual([1,3,2], res)
+
+    def test_nextPermutation_1(self):
+        solution = Solution()
+        res = solution.nextPermutation([3,2,1])
+        self.assertEqual([1,2,3], res)
+
+    def test_nextPermutation_2(self):
+        solution = Solution()
+        res = solution.nextPermutation([6,2,1,5,4,3,0])
+        self.assertEqual([6,2,3,0,1,4,5], res)
+
+    def test_nextPermutation_3(self):
+        solution = Solution()
+        res = solution.nextPermutation([5,1,1])
+        self.assertEqual([1,1,5], res)
