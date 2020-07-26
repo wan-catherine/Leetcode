bc68fc30da265183fa595c5118b1dbced9bdb47a
wanwan <rarry2012@gmail.com>
2020-07-26 08:23:56 -0400

    update

--- problems/AddDigits.py
+++ problems/AddDigits.py
@@ -1,7 +1,23 @@
+"""
+Digit root:
+N=(a[0] * 1 + a[1] * 10 + ...a[n] * 10 ^n),and a[0]...a[n] are all between [0,9]
+we set M = a[0] + a[1] + ..a[n]
+and another truth is that:
+1 % 9 = 1
+10 % 9 = 1
+100 % 9 = 1
+so N % 9 = a[0] + a[1] + ..a[n]
+means N % 9 = M
+so N = M (% 9)
+as 9 % 9 = 0,so we can make (n - 1) % 9 + 1 to help us solve the problem when n is 9.as N is 9, ( 9 - 1) % 9 + 1 = 9
+"""
 class Solution:
     def addDigits(self, num):
         """
         :type num: int
         :rtype: int
         """
-        return num - 9 * (int)((num-1)/9)
+        if num == 0:
+            return 0
+
+        return (num - 1) % 9 + 1
