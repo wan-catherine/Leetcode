54ac58c032a5e279bda18c904cb91c685fe5b64b
wanwan <rarry2012@gmail.com>
2020-08-07 13:42:43 -0400

    987

--- problems/N987_Vertical_Order_Traversal_To_A_Binary_Tree.py
+++ problems/N987_Vertical_Order_Traversal_To_A_Binary_Tree.py
@@ -1,3 +1,5 @@
+import collections
+
 from problems.Utility_Tree import TreeNode
 
 class Solution(object):
@@ -6,4 +8,22 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        
+        self.x_y_mapping = collections.defaultdict(list)
+        self.helper(root, 0 , 0)
+        res = []
+
+        keys = sorted(self.x_y_mapping.keys())
+        for x in keys:
+            # here is the key part , sort by multiple keys
+            # use -x[1] same as reverse, but it much faster
+            self.x_y_mapping[x].sort(key=lambda x: (-x[1], x[0]))
+            x_level = [val for val, y in self.x_y_mapping[x]]
+            res.append(x_level)
+        return res
+
+    def helper(self, node, x, y):
+        self.x_y_mapping[x].append((node.val, y))
+        if node.left:
+            self.helper(node.left, x-1, y-1)
+        if node.right:
+            self.helper(node.right, x+1, y-1)
