78f55542b17e171ba72f930a4103806af2dafdc3
wanwan <rarry2012@gmail.com>
2020-08-27 17:15:07 -0400

    430

--- problems/N430_Flatten_A_Multilevel_Doubly_Linked_List.py
+++ problems/N430_Flatten_A_Multilevel_Doubly_Linked_List.py
@@ -8,7 +8,7 @@ class Node(object):
 
 
 class Solution(object):
-    def flatten(self, head):
+    def flatten_before(self, head):
         """
         :type head: Node
         :rtype: Node
@@ -38,3 +38,41 @@ class Solution(object):
         dummy.next.prev = None
 
         return head
+
+    def flatten(self, head):
+        if not head:
+            return head
+
+        cur = head
+        self.dfs(cur)
+        return head
+
+    """
+    Spent a lot of time one this function. 
+    Three key points:
+        1. return the tail node 
+        2. when node has child, first find the child level's trail node 
+        3. after from child to the last level, still remember to call self.dfs
+    """
+    def dfs(self, node):
+        if node.child:
+            last_node = self.dfs(node.child)
+            next_node = node.next
+            child = node.child
+            node.next = child
+            child.prev = node
+            node.child = None
+            last_node.next = next_node
+            if next_node:
+                next_node.prev = last_node
+                return self.dfs(next_node)
+            else:
+                return last_node
+        elif node.next:
+            return self.dfs(node.next)
+        else:
+            return node
+
+
+
+
