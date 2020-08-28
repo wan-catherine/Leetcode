1d8d1b09f9c7b5c5cc48c7f2e45a9b5e7846e0f2
wanwan <rarry2012@gmail.com>
2020-08-27 20:24:03 -0400

    332

--- problems/N332_Reconstruct_Itinerary.py
+++ problems/N332_Reconstruct_Itinerary.py
@@ -2,7 +2,7 @@ import collections
 
 
 class Solution(object):
-    def findItinerary(self, tickets):
+    def findItinerary_before(self, tickets):
         """
         :type tickets: List[List[str]]
         :rtype: List[str]
@@ -16,10 +16,10 @@ class Solution(object):
         for key in graph:
             graph[key].sort()
 
-        res = self.dfs(graph, 'JFK', [], visited)
+        res = self.dfs_before(graph, 'JFK', [], visited)
         return res
 
-    def dfs(self, graph, start, arr, visited):
+    def dfs_before(self, graph, start, arr, visited):
         arr.append(start)
         if start not in graph:
             return
@@ -32,4 +32,63 @@ class Solution(object):
                 return arr
             visited[(start, end)] += 1
             arr.pop()
+
+    def findItinerary(self, tickets):
+        if not tickets:
+            return None
+        self.length = len(tickets)
+        self.graph = collections.defaultdict(list)
+        self.visited = collections.defaultdict(int)
+        for f, t in tickets:
+            self.graph[f].append(t)
+            self.visited[(f,t)] += 1
+        res = ['JFK']
+        self.dfs('JFK', res, 0)
+        return res
+
+
+    def dfs(self, f, res, count):
+        if f not in self.graph:
+            return
+
+        for t in sorted(self.graph[f]):
+            if not self.visited[(f,t)]:
+                continue
+            self.visited[(f,t)] -= 1
+            res.append(t)
+            count += 1
+            self.dfs(t, res, count)
+            if len(res) == self.length + 1:
+                return
+
+            res.pop()
+            self.visited[(f,t)] += 1
+
+    def findItinerary(self, tickets):
+        graph = collections.defaultdict(list)
+        deg_out = collections.defaultdict(int)
+
+        # construct adjency list
+        for frm, to in tickets:
+            graph[frm].append(to)
+            deg_out[frm] += 1
+
+        path = []
+        for k, v in graph.items():
+            graph[k] = sorted(v, reverse=True)
+
+        def dfs(node):
+            while graph[node]:
+                curr = graph[node].pop()
+                dfs(curr)
+            path.append(node)
+
+        dfs("JFK")
+        return reversed(path)
+
+
+
+
+
+
 
