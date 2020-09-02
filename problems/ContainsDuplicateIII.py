d80ac73dce805116e60e5475eaf9a9ae37ffcd9f
wanwan <rarry2012@gmail.com>
2020-09-02 14:14:06 -0400

    220

--- problems/ContainsDuplicateIII.py
+++ problems/ContainsDuplicateIII.py
@@ -1,5 +1,8 @@
+import collections
+
+
 class Solution:
-    def containsNearbyAlmostDuplicate(self, nums, k, t):
+    def containsNearbyAlmostDuplicate_before(self, nums, k, t):
         """
         :type nums: List[int]
         :type k: int
@@ -9,6 +12,7 @@ class Solution:
         length = len(nums)
         if nums == None or length < 1 :
             return False
+        #this lien is the key part to pass all test cases and avoid TLE!!!
         if len(set(nums)) == length and t == 0:
             return False
 
@@ -31,5 +35,47 @@ class Solution:
         for i in range(1, len(nums)):
             if nums[i] - nums[i-1] <= t:
                 return True
+        return False
+
+    def containsNearbyAlmostDuplicate_before(self, nums, k, t):
+        if not nums:
+            return False
+        length = len(nums)
+        if length < 2:
+            return False
+        if len(set(nums)) == length and t == 0:
+            return False
+        start, end = 0, 1
+        while start < length:
+            if end - start <= k:
+                p = start + 1
+                while p <= end:
+                    if abs(nums[p] - nums[start]) <= t:
+                        return True
+                    p += 1
+            if end - start == k or end == length - 1:
+                start += 1
+            if end < length - 1:
+                end += 1
+            # print(start, end)
+        return False
+
+    def containsNearbyAlmostDuplicate(self, nums, k, t):
+        if t < 0:
+            return False
+        length = len(nums)
+        buckets = collections.OrderedDict()
+        model = t + 1
+        for i in range(length):
+            key = nums[i] // model
+            if key in buckets:
+                return True
+            if key - 1 in buckets and abs(buckets[key-1]-nums[i]) < model:
+                return True
+            if key + 1 in buckets and abs(buckets[key+1]-nums[i]) < model:
+                return True
+            buckets[key] = nums[i]
+            if i >= k:
+                del buckets[nums[i-k] // model]
         return False
 
