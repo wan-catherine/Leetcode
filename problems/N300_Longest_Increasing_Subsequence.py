07e1a76d70bf1e0082b6205099d8a5c4bf361153
wanwan <rarry2012@gmail.com>
2020-09-15 15:55:48 -0400

    300

--- problems/N300_Longest_Increasing_Subsequence.py
+++ problems/N300_Longest_Increasing_Subsequence.py
@@ -23,12 +23,31 @@ class Solution(object):
                 res = max(res, maximum)
         return res + 1
 
+    # update 20200915
+    def lengthOfLIS_myself(self, nums):
+        res = 0
+        stack = []
+        mapping = {}
+        for i, c in enumerate(nums):
+            index = bisect.bisect_left(stack, c)
+            bisect.insort_left(stack, c)
+            if index == 0:
+                mapping[c] = 1
+                res = max(res, 1)
+                continue
+            count = 1
+            for j in range(index):
+                count = max(count, mapping[stack[j]] + 1)
+            mapping[c] = count
+            res = max(res, count)
+        return res
+
     """
     arr is the an array which is the longest increasing subsequence.
     when there is a new n , which is larger than arr[-1], then append it to arr .
     when there is a new n, whhich is smaller than arr[-1], find the smallest num in arr which larger than n. 
     then use n to replace this number . 
-    This way, we can keep all numbers in array are as smaller as possble while keep the lenght same. 
+    This way, we can keep all numbers in array are as smaller as possble while keep the length same. 
     URL: https://www.youtube.com/watch?v=Q6KyDl_xiIg
     """
     def lengthOfLIS(self, nums):
