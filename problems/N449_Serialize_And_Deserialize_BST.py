6ad2e731493550b86fbe254675356a404689936e
wanwan <rarry2012@gmail.com>
2020-10-06 15:22:24 -0400

    449

--- problems/N449_Serialize_And_Deserialize_BST.py
+++ problems/N449_Serialize_And_Deserialize_BST.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-from .Utility_Tree import TreeNode
+from Utility_Tree import TreeNode, list_to_tree_node, null, treenode_to_list
 class Codec:
 
     def serialize(self, root):
@@ -9,9 +9,9 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        inorder = self.get_inorder(root)
         preorder = self.get_preorder(root)
-        return ''.join(preorder+inorder)
+        inorder = self.get_inorder(root)
+        return ','.join([str(i) for i in preorder+inorder])
 
     def get_inorder(self, root):
         if not root:
@@ -23,14 +23,28 @@ class Codec:
             node = stack[-1]
             if node.left:
                 stack.append(node.left)
+                node.left = None
             else:
                 res.append(node.val)
-                stack.popleft()
+                stack.pop()
                 if node.right:
                     stack.append(node.right)
+                    node.right = None
         return res
 
-    def get_
+    def get_preorder(self, root):
+        res = []
+        if not root:
+            return res
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
 
 
     def deserialize(self, data):
@@ -39,3 +53,27 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        if not data:
+            return None
+
+        data = [int(i) for i in data.split(',')]
+        length = len(data) // 2
+        preorder, inorder = data[:length], data[length:]
+        return self.create_tree(preorder, inorder)
+
+    def create_tree(self, preorder, inorder):
+        if not preorder:
+            return
+        root = TreeNode(preorder[0])
+        index = inorder.index(preorder[0])
+        root.left = self.create_tree(preorder[1:index+1], inorder[:index])
+        root.right = self.create_tree(preorder[index+1:], inorder[index+1:])
+        return root
+
+ser = Codec()
+deser = Codec()
+root = list_to_tree_node([41,37,44,24,39,42,48,1,35,38,40,null,43,46,49,0,2,30,36,null,null,null,null,null,null,45,47,null,null,null,null,null,4,29,32,null,null,null,null,null,null,3,9,26,null,31,34,null,null,7,11,25,27,null,null,33,null,6,8,10,16,null,null,null,28,null,null,5,null,null,null,null,null,15,19,null,null,null,null,12,null,18,20,null,13,17,null,null,22,null,14,null,null,21,23])
+tree = ser.serialize(root)
+print(tree)
+ans = deser.deserialize(tree)
+print(treenode_to_list(ans))
--- test/test_binary_tree_preorder_traversal.py
+++ test/test_binary_tree_preorder_traversal.py
@@ -1,11 +1,13 @@
 from unittest import TestCase
 from problems.BinaryTreePreorderTraversal import Solution, TreeNode
+from problems.Utility_Tree import list_to_tree_node, null
 
 class TestSolution(TestCase):
     def test_preorderTraversal(self):
-        root = TreeNode(1)
-        root.right = TreeNode(2)
-        root.right.left = TreeNode(3)
-        solution = Solution()
-        res = solution.preorderTraversal(root)
-        self.assertEqual([1,2,3], res)
+        self.assertListEqual([1,2,3], Solution().preorderTraversal(list_to_tree_node([1,null,2,3])))
+
+    def test_preorderTraversal_1(self):
+        self.assertListEqual([3,1,2], Solution().preorderTraversal(list_to_tree_node([3,1,2])))
+
+    def test_preorderTraversal_2(self):
+        self.assertListEqual([1,4,2,3], Solution().preorderTraversal(list_to_tree_node([1,4,3,2])))
