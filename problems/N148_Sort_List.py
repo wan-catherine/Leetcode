5d5a016d4de05a492b8039b7f22c9b0ef4b9a9cc
wanwan <rarry2012@gmail.com>
2020-10-13 14:08:46 -0400

    148

--- problems/N148_Sort_List.py
+++ problems/N148_Sort_List.py
@@ -13,7 +13,7 @@ class ListNode(object):
         self.next = next
 
 class Solution(object):
-    def sortList(self, head):
+    def sortList_before(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
@@ -32,7 +32,7 @@ class Solution(object):
             length += 1
         return temp, length
 
-    def merge(self, head, end, length):
+    def merge_(self, head, end, length):
         if head == end:
             return head
 
@@ -67,6 +67,84 @@ class Solution(object):
             temp.next = right
         return head.next
 
+    def sortList_recursion(self, head):
+        if not head or not head.next:
+            return head
 
+        fast, slow = head.next, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
 
+        def merge(l1, l2):
+            head = temp = ListNode()
+            while l1 and l2:
+                if l1.val > l2.val:
+                    temp.next = l2
+                    l2 = l2.next
+                else:
+                    temp.next = l1
+                    l1 = l1.next
+                if not head:
+                    head = temp
+                temp = temp.next
+            if l1:
+                temp.next = l1
+            if l2:
+                temp.next = l2
+            return head.next
 
+        mid = slow.next
+        slow.next = None
+        return merge(self.sortList(head), self.sortList(mid))
+
+    def sortList(self, head):
+        def merge(l1, l2, head):
+            cur = head
+            while l1 and l2:
+                if l1.val > l2.val:
+                    cur.next = l2
+                    l2 = l2.next
+                else:
+                    cur.next = l1
+                    l1 = l1.next
+                cur = cur.next
+            cur.next = l1 if l1 else l2
+            while cur.next:
+                cur = cur.next
+            return cur
+
+        def split(head, n):
+            while n-1 and head:
+                head = head.next
+                n -= 1
+
+            if not head:
+                return None
+
+            new_head = head.next
+            head.next = None
+            return new_head
+
+        if not head or not head.next:
+            return head
+
+        cur = head
+        length = 0
+        while cur:
+            length += 1
+            cur = cur.next
+
+        dummy = ListNode(0)
+        dummy.next = head
+        step = 1
+        while step < length:
+            cur = dummy.next
+            tail = dummy
+            while cur:
+                left = cur
+                right = split(left, step)
+                cur = split(right, step)
+                tail = merge(left, right, tail)
+            step = step << 1
+        return dummy.next
