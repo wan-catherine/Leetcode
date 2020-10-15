f5344414aa1466ff74fdb35341ce33546411b8f8
wanwan <rarry2012@gmail.com>
2020-10-15 15:53:43 -0400

    rotate

--- problems/RotateArray.py
+++ problems/RotateArray.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def rotate(self, nums, k):
+    def rotate_old(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
@@ -11,8 +11,18 @@ class Solution(object):
 
         if l == 0:
             return
-
-        # newNums = nums[-1 * l :] + nums[0:len(nums) - l]
-        # nums.clear()
-        # nums.extend(newNums)
         nums[:] = nums[-1 * l :] + nums[0:len(nums) - l]
+
+    def rotate(self, nums, k):
+        length = len(nums)
+        k = k % length
+
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        reverse(0, length - 1)
+        reverse(0, k-1)
+        reverse(k, length-1)
