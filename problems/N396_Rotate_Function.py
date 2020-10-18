5beed123760f7295dc132d49a3f056d6724f9d41
wanwan <rarry2012@gmail.com>
2020-10-18 19:35:48 -0400

    396

--- problems/N396_Rotate_Function.py
+++ problems/N396_Rotate_Function.py
@@ -1,10 +1,10 @@
 """
-f(i)          = 0 * A[0] + 1 * A[1] + 2 * A[2] + .... +  (k-1) * A[k-1] + k * A[k]
-  f(i+1)        = 1 * A[0] + 2 * A[1] + 3 * A[2] + ...  +     k  * A[k-1] + 0 * A[k]
-=>f(i+1) - f(i) =     A[0]   +   A[1]   +   A[2] + ...  +       A[k-1]    - k * A[k]
-   = (A[0] + A[1] + A[2] + ... + A[k-1] + A[k]) - (k+1) * A[k]
-   = sum(Array) - A[k] * array.length
-=> f(i+1) = f(i) + sum(Array) -  last element * array.length
+f(i) = 0 * A[0] + 1 * A[1] + 2 * A[2] + .... + (k-1) * A[k-1] + k * A[k]
+f(i+1) = 1 * A[0] + 2 * A[1] + 3 * A[2] + ... + k * A[k-1] + 0 * A[k]
+=>f(i+1) - f(i) = A[0] + A[1] + A[2] + ... + A[k-1] - k * A[k]
+                = (A[0] + A[1] + A[2] + ... + A[k-1] + A[k]) - (k+1) * A[k]
+                = sum(Array) - A[k] * array.length
+=> f(i+1) = f(i) + sum(Array) - last element * array.length
 
 Here the A[k] will be the reverse of A.
 """
