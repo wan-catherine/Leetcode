dba1c5f4f11e5f60e99880b2a2a92fc3560a44a4
wanwan <rarry2012@gmail.com>
2020-10-31 11:39:54 -0400

    99

--- problems/N99_Recover_Binary_Search_Tree.py
+++ problems/N99_Recover_Binary_Search_Tree.py
@@ -5,7 +5,7 @@ Key point :
 """
 from .Utility_Tree import TreeNode
 class Solution(object):
-    def recoverTree(self, root):
+    def recoverTree_dfs(self, root):
         """
         :type root: TreeNode
         :rtype: None Do not return anything, modify root in-place instead.
@@ -33,3 +33,26 @@ class Solution(object):
         dfs(root)
         first.val, second.val = second.val, first.val
 
+    def recoverTree(self, root):
+        arr = [root]
+        vals = [TreeNode(-2 ** 31 - 1)]
+        visited = set()
+        first, second = None, None
+        while arr:
+            node = arr[-1]
+            if node.left and node.left not in visited:
+                arr.append(node.left)
+                continue
+            if node.val < vals[-1].val:
+                if not first:
+                    first = vals[-1]
+                    second = node   # need to set second here , or for test_recoverTree_1, it will failed for second is None
+                else:
+                    second = node
+            vals.append(node)
+            visited.add(node)
+            arr.pop()
+            if node.right and node.right not in visited:
+                arr.append(node.right)
+                continue
+        first.val, second.val = second.val, first.val
