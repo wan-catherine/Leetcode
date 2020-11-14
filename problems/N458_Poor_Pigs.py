8ece03d3573fe64d054bc793ba781bb0736ff06f
wanwan <rarry2012@gmail.com>
2020-11-14 09:11:06 -0500

    458

--- problems/N458_Poor_Pigs.py
+++ problems/N458_Poor_Pigs.py
@@ -1,7 +1,7 @@
 """
 Notice, a pig can drink more than one bucket at the same time.
 For each one pig, we can have val = (minutesToTest // minutesToDie + 1) status .
-So for n pigs, we can have val ** n status. 
+So for n pigs, we can have val ** n status.
 """
 class Solution(object):
     def poorPigs(self, buckets, minutesToDie, minutesToTest):
