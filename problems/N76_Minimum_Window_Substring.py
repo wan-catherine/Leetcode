bf1d00b4a59b3566e33a69add11b2532e6481fa6
wanwan <rarry2012@gmail.com>
2020-11-26 13:50:27 -0500

    76

--- problems/N76_Minimum_Window_Substring.py
+++ problems/N76_Minimum_Window_Substring.py
@@ -1,5 +1,8 @@
+import collections
+
+
 class Solution:
-    def minWindow(self, s, t):
+    def minWindow_before(self, s, t):
         """
         :type s: str
         :type t: str
@@ -38,7 +41,40 @@ class Solution:
                 i += 1
         return res
 
+    """
+    Slide window 
+    The key point is how to check the substring already contains t. 
+    We use hashmap and correct_num . 
+    correct_num shows how many unique character already have no less than t's number. 
+    """
+    def minWindow(self, s: str, t: str) -> str:
+        count = collections.defaultdict(int)
+        for c in t:
+            count[c] += 1
+        sl, tl = len(s), len(count.keys())
+        left, right = 0, 0
+        valid = collections.defaultdict(int)
+        correct_num = 0
+        res = ""
+        while right < sl:
+            if s[right] in count.keys():
+                valid[s[right]] += 1
+                if valid[s[right]] == count[s[right]]:
+                    correct_num += 1
 
+                while correct_num == tl:
+                    if not res:
+                        res = s[left:right + 1]
+                    else:
+                        res = s[left:right + 1] if right - left + 1 < len(res) else res
+
+                    if s[left] in count.keys():
+                        valid[s[left]] -= 1
+                        if valid[s[left]] < count[s[left]]:
+                            correct_num -= 1
+                    left += 1
+            right += 1
+        return res
 
 
 
