9a8bb77c8ddd5340ad5d5814735714cd97e18179
wanwan <rarry2012@gmail.com>
2020-12-03 14:39:14 -0500

    659

--- problems/N659_Split_Array_Into_Consecutive_Subsequences.py
+++ problems/N659_Split_Array_Into_Consecutive_Subsequences.py
@@ -1,4 +1,5 @@
 import collections
+import heapq
 
 """
 Use two dictionary:
@@ -9,7 +10,7 @@ We create 3 sequence first , then write the 4th number into the next_num_mapping
 
 """
 class Solution(object):
-    def isPossible(self, nums):
+    def isPossible_(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
@@ -36,4 +37,46 @@ class Solution(object):
                 num_count_mapping[num+i] -= 1
             next_num_mapping[num+3] += 1
 
+        return True
+
+    # O(nlgn)
+    def isPossible_hashtable(self, nums):
+        tails = collections.defaultdict(list)
+        for n in nums:
+            count = 0
+            if n - 1 in tails:
+                count = heapq.heappop(tails[n - 1])
+                if not tails[n - 1]:
+                    del tails[n - 1]
+            count += 1
+            if n not in tails:
+                tails[n] = []
+                heapq.heappush(tails[n], count)
+            else:
+                heapq.heappush(tails[n], count)
+        print(tails)
+        for key, li in tails.items():
+            if li[0] < 3:
+                return False
+        return True
+
+    def isPossible(self, nums):
+        counter = collections.Counter(nums)
+        end = collections.defaultdict(int)
+
+        for n in nums:
+            if counter[n] == 0:
+                continue
+            if end[n-1] > 0:
+                end[n-1] -= 1
+                counter[n] -= 1
+                end[n] += 1
+            else:
+                if counter[n+1] > 0 and counter[n+2] > 0:
+                    counter[n+1] -= 1
+                    counter[n] -= 1
+                    counter[n+2] -= 1
+                    end[n+2] += 1
+                else:
+                    return False
         return True
--- test/test_659_split_array_into_consecutive_subsequences.py
+++ test/test_659_split_array_into_consecutive_subsequences.py
@@ -10,3 +10,6 @@ class TestSolution(TestCase):
 
     def test_isPossible_2(self):
         self.assertFalse(Solution().isPossible([1,2,3,4,4,5]))
+
+    def test_isPossible_3(self):
+        self.assertTrue(Solution().isPossible([1,2,3,4,6,7,8,9,10,11]))
