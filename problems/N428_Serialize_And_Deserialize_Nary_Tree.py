b74ef3847045ef90b143b67af9d4c9fd8a140f02
wanwan <rarry2012@gmail.com>
2020-12-24 13:14:08 -0500

    428

---
+++ problems/N428_Serialize_And_Deserialize_Nary_Tree.py
@@ -0,0 +1,48 @@
+
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+class Codec:
+    def serialize(self, root: 'Node') -> str:
+        """Encodes a tree to a single string.
+
+        :type root: Node
+        :rtype: str
+        """
+        res = []
+
+        def dfs(node):
+            if not node:
+                return
+            res.append(str(node.val))
+            res.append(str(len(node.children)))
+            for child in node.children:
+                dfs(child)
+        dfs(root)
+        return ','.join(res)
+
+    def deserialize(self, data: str) -> 'Node':
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: Node
+        """
+        if not data:
+            return None
+
+        data = iter(data.split(','))
+
+        def dfs():
+            root = Node(int(next(data)), [])
+            count = int(next(data))
+            for _ in range(count):
+                root.children.append(dfs())
+            return root
+
+        return dfs()
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
