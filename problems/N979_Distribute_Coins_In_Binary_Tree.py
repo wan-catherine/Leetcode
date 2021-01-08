945d729af9fdb06e7aac0ab40cecf68c8064e70e
wanwan <rarry2012@gmail.com>
2021-01-08 09:57:28 -0500

    979

--- problems/N979_Distribute_Coins_In_Binary_Tree.py
+++ problems/N979_Distribute_Coins_In_Binary_Tree.py
@@ -2,10 +2,10 @@
 Consider the flow on the edge .
 for a node , how many coins does it have to make his left and right children balance?
 if a child has more coins, it needs to move node.val - 1 to his parents , if a child has less than one coins,
-it needs get some coins from his parent. 
+it needs get some coins from his parent.
 """
 class Solution(object):
-    def distributeCoins(self, root):
+    def distributeCoins_(self, root):
         """
         :type root: TreeNode
         :rtype: int
@@ -21,3 +21,25 @@ class Solution(object):
 
         dfs(root, res)
         return res[0]
+
+    # update at 20210108
+    def distributeCoins(self, root) -> int:
+        res = 0
+
+        def dfs(node):
+            nonlocal res
+            if not node.left and not node.right:
+                need = node.val - 1
+                res += abs(need)
+                return need
+
+            val = node.val
+            if node.left:
+                val += dfs(node.left)
+            if node.right:
+                val += dfs(node.right)
+            res += abs(val - 1)
+            return val - 1
+
+        dfs(root)
+        return res
