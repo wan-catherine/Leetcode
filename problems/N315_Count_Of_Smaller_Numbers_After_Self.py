0d5974069e8ba47bb3b958937d7c84a5219ab843
wanwan <rarry2012@gmail.com>
2021-01-11 14:10:07 -0500

    315

--- problems/N315_Count_Of_Smaller_Numbers_After_Self.py
+++ problems/N315_Count_Of_Smaller_Numbers_After_Self.py
@@ -8,7 +8,7 @@ We can write binary search by ourself, but need to deal with duplicated number.
 """
 
 class Solution(object):
-    def countSmaller(self, nums):
+    def countSmaller_(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
@@ -23,4 +23,43 @@ class Solution(object):
             index = bisect.bisect_left(temp,nums[i])
             bisect.insort_left(temp, nums[i])
             res[i] = index
+        return res
+
+    def countSmaller(self, nums):
+        original = nums[:]
+        length = len(nums)
+        res = [0] * length
+
+        def helper(s, e):
+            if s+1 >= e:
+                return
+
+            m = (e - s) // 2 + s
+            helper(s, m)
+            helper(m, e)
+
+            for i in range(s, m):
+                index = bisect.bisect_left(nums[m:e], original[i]) #here we must use original[i] to do search, because nums already changed
+                res[i] += index
+
+            l, r = nums[s:m], nums[m:e]
+            i, j, k = 0, 0, s
+            while i < len(l) and j < len(r):
+                if l[i] > r[j]:
+                    nums[k] = r[j]
+                    j += 1
+                else:
+                    nums[k] = l[i]
+                    i += 1
+                k += 1
+            while i < len(l):
+                nums[k] = l[i]
+                i += 1
+                k += 1
+            while j < len(r):
+                nums[k] = r[j]
+                j += 1
+                k += 1
+
+        helper(0, length)
         return res
--- test/test_315_count_of_smaller_numbers_after_self.py
+++ test/test_315_count_of_smaller_numbers_after_self.py
@@ -7,3 +7,9 @@ class TestSolution(TestCase):
 
     def test_countSmaller_1(self):
         self.assertListEqual([0,0], Solution().countSmaller([-1,-1]))
+
+    def test_countSmaller_2(self):
+        self.assertListEqual([0,3,1,1,0], Solution().countSmaller([1,9,7,8,5]))
+
+    def test_countSmaller_3(self):
+        self.assertListEqual([10,27,10,35,12,22,28,8,19,2,12,2,9,6,12,5,17,9,19,12,14,6,12,5,12,3,0,10,0,7,8,4,0,0,4,3,2,0,1,0], Solution().countSmaller([26,78,27,100,33,67,90,23,66,5,38,7,35,23,52,22,83,51,98,69,81,32,78,28,94,13,2,97,3,76,99,51,9,21,84,66,65,36,100,41]))
