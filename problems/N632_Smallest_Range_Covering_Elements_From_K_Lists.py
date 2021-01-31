e1cc7341d2166bed8ce688b37bd76014c059401b
wanwan <rarry2012@gmail.com>
2021-01-30 19:14:55 -0500

    1632

--- problems/N632_Smallest_Range_Covering_Elements_From_K_Lists.py
+++ problems/N632_Smallest_Range_Covering_Elements_From_K_Lists.py
@@ -1,9 +1,11 @@
 import bisect
+import heapq
+import sys
 from collections import deque
 
 
 class Solution(object):
-    def smallestRange(self, nums):
+    def smallestRange_before(self, nums):
         """
         :type nums: List[List[int]]
         :rtype: List[int]
@@ -26,3 +28,29 @@ class Solution(object):
             if arr[-1][0] - arr[0][0] < res[1] - res[0]:
                 res = [arr[0][0], arr[-1][0]]
 
+    """
+    update at 20210130
+    similar as 1675.
+    """
+    def smallestRange(self, nums):
+        k = len(nums)
+        arr = []
+        index = [0] * k
+        minimum, maximum = sys.maxsize, -sys.maxsize
+        for i, li in enumerate(nums):
+            arr.append((li[0], i))
+            minimum = min(minimum, li[0])
+            maximum = max(maximum, li[0])
+        res = [minimum, maximum]
+        heapq.heapify(arr)
+        while True:
+            val, idx = heapq.heappop(arr)
+            if index[idx] + 1 == len(nums[idx]):
+                break
+            index[idx] += 1
+            heapq.heappush(arr, (nums[idx][index[idx]], idx))
+            minimum = arr[0][0]
+            maximum = max(maximum, nums[idx][index[idx]])
+            if maximum - minimum < res[1] - res[0]:
+                res[0], res[1] = minimum, maximum
+        return res
