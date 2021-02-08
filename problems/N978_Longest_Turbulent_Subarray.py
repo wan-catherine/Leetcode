75463f3395227a7744527a0c98a5a1f0810a00f3
wanwan <rarry2012@gmail.com>
2021-02-07 19:50:03 -0500

    978

--- problems/N978_Longest_Turbulent_Subarray.py
+++ problems/N978_Longest_Turbulent_Subarray.py
@@ -9,7 +9,7 @@ A[i] > A[i-1] : dp_h[i] = dp_l[i-1] + 1, dp_l[i] = 1
 A[i] < A[i-1] : dp_h[i] = 1, dp_l[i] = dp_h[i-1] + 1
 """
 class Solution(object):
-    def maxTurbulenceSize(self, A):
+    def maxTurbulenceSize_dp(self, A):
         """
         :type A: List[int]
         :rtype: int
@@ -28,4 +28,33 @@ class Solution(object):
             else:
                 dp_l[i], dp_h[i] = 1, 1
             res = max(dp_h[i], dp_l[i], res)
+        return res
+
+    def maxTurbulenceSize(self, arr):
+        length = len(arr)
+        if length == 1:
+            return 1
+        start, end = 0, 1
+        prev = None
+        res = 1
+        while end < length:
+            if prev == None:
+                if arr[end] == arr[end - 1]:
+                    start += 1
+                    end += 1
+                    continue
+                prev = True if arr[end] > arr[end - 1] else False
+                end += 1
+            else:
+                if arr[end] == arr[end - 1]:
+                    start = end
+                    end += 1
+                    continue
+                temp = True if arr[end] > arr[end - 1] else False
+                if temp == (not prev):
+                    prev = temp
+                else:
+                    start = end - 1
+                end += 1
+            res = max(res, end - start)
         return res
