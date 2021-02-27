4c2c59033626aa78279ddf6a00e60e0055e88d76
wanwan <rarry2012@gmail.com>
2021-02-26 22:55:28 -0500

    813

--- problems/N813_Largest_Sum_Of_Averages.py
+++ problems/N813_Largest_Sum_Of_Averages.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def largestSumOfAverages(self, A, K):
+    def largestSumOfAverages_dfs(self, A, K):
         memo = {}
         length = len(A)
 
@@ -20,3 +20,24 @@ class Solution(object):
             return value
 
         return dfs(0, K)
+
+    """
+    dp[i][j] : the largest score we can get by splitting arr[i:] into j groups. 
+    dp[i][j] = max(dp[i][j], dp[k][j-1]) k is the index for arr[i+1:]
+    """
+    def largestSumOfAverages(self, arr, K: int) -> float:
+        length = len(arr)
+        prefix = [0] + arr[:]
+        for i in range(1, length):
+            prefix[i + 1] += prefix[i]
+
+        dp = [[0] * (K+1) for _ in range(length)]
+        for i in range(length):
+            dp[i][1] = (prefix[-1] - prefix[i]) / (length - i)
+
+        for i in range(length - 1, -1, -1):
+            for j in range(2, min(length - i, K) + 1):
+                for k in range(i + 1, length - j + 2):
+                    dp[i][j] = max(dp[i][j], dp[k][j - 1] + (prefix[k] - prefix[i]) / (k - i))
+
+        return max(dp[0])
