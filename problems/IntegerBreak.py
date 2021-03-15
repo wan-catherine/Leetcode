d62290037ae718ddd44009438dc9d378cf86d119
wanwan <rarry2012@gmail.com>
2021-03-15 14:25:49 -0400

    343

--- problems/IntegerBreak.py
+++ problems/IntegerBreak.py
@@ -18,3 +18,17 @@ class Solution:
             res = max(temp, res)
             i += 1
         return res
+
+    def integerBreak_dp(self, n: int) -> int:
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+        dp = [0] * (n + 1)
+        # base caee : at least i//2 >= 2 , so we need dp[2], dp[3] here .
+        dp[2] = 2
+        dp[3] = 3
+        for i in range(4, n + 1):
+            for j in range(2, i // 2 + 1):
+                dp[i] = max(dp[i], dp[j] * dp[i - j])
+        return dp[-1]
