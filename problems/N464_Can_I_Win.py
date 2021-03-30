5069033d14e855cb782badfeed95f5a13e1636ad
wanwan <rarry2012@gmail.com>
2021-03-30 12:07:40 -0400

    464

--- problems/N464_Can_I_Win.py
+++ problems/N464_Can_I_Win.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def canIWin(self, maxChoosableInteger, desiredTotal):
+    def canIWin_(self, maxChoosableInteger, desiredTotal):
         """
         :type maxChoosableInteger: int
         :type desiredTotal: int
@@ -28,6 +28,36 @@ class Solution(object):
                 self.status[state] = 1
                 return True
         self.status[state] = -1
+        return False
+
+    def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
+        # need to check this, or it will fail for (5, 50)
+        max_sum = sum([i for i in range(1, maxChoosableInteger + 1)])
+        if max_sum < desiredTotal:
+            return False
+
+        memo = {}
+
+        def dfs(state, cur):
+            nonlocal desiredTotal
+            # for (5, 50), it will return True.so need to check before do dfs
+            if cur >= desiredTotal:
+                return True
+            if (state, cur) in memo:
+                return memo[(state, cur)]
+            flag = False
+            for i in range(1, maxChoosableInteger + 1):
+                if state & (1 << i):
+                    continue
+                if dfs(state | (1 << i), cur + i):
+                    flag = True
+                    break
+            memo[(state, cur)] = not flag
+            return not flag
+
+        for i in range(1, maxChoosableInteger + 1):
+            if dfs(0 | (1 << i), i):
+                return True
         return False
 
 
