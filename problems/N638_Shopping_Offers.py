2b1f8e254bf590eb80567ed919079bdfee21338f
wanwan <rarry2012@gmail.com>
2021-04-01 21:53:53 -0400

    638

--- problems/N638_Shopping_Offers.py
+++ problems/N638_Shopping_Offers.py
@@ -1,8 +1,9 @@
 from math import inf
+from typing import List
 
 
 class Solution(object):
-    def shoppingOffers(self, price, special, needs):
+    def shoppingOffers_(self, price, special, needs):
         """
         :type price: List[int]
         :type special: List[List[int]]
@@ -46,3 +47,38 @@ class Solution(object):
         value = min(value, cost)
         self.memo[key] = value
         return value
+
+    """
+    update : 20210401
+    """
+    def shoppingOffers(self, price: List[int], special: List[List[int]], needs: List[int]) -> int:
+        lp = len(price)
+        needs = tuple(needs)
+        memo = {}
+        def dfs(state):
+            nonlocal lp
+            if state in memo:
+                return memo[state]
+            if sum(state) == 0:
+                return 0
+            temp = list(state)
+            val = sum(price[i]*state[i] for i in range(lp))
+            for li in special:
+                for i in range(lp):
+                    if temp[i] < li[i]:
+                        temp = list(state)  # here need to recover temp !!!
+                        break
+                    else:
+                        temp[i] -= li[i]
+                else:
+                    val = min(val, dfs(tuple(temp)) + li[-1])
+                    temp = list(state)
+                # new_state = [state[i] - li[i] for i in range(lp)]
+                # if min(new_state) >= 0:
+                #     val = min(val, dfs(tuple(new_state)) + li[-1])
+            memo[(state)] = val
+            return val
+
+        res = dfs(tuple(needs))
+        # print(memo)
+        return res
