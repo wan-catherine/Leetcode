b8bf0991b218f40f731df077c85cd7d78501d356
wanwan <rarry2012@gmail.com>
2021-04-29 14:00:54 -0400

    137

--- problems/SingleNumberII.py
+++ problems/SingleNumberII.py
@@ -6,11 +6,14 @@ class Solution(object):
         """
         return (3*sum(set(nums)) - sum(nums)) / 2
 
+    """
+    python negative number need to ~(res ^ 0xffffffff)
+    """
     def singleNumber(self, nums):
         number, res = 0, 0
-        for i in range(64):
+        for i in range(32):
             number = 0
             for k in nums:
                 number += (k >> i) & 1
             res |= (number) % 3 << i
-        return res
+        return res if (number % 3) == 0 else ~(res ^ 0xffffffff)
--- test/test_single_number_ii.py
+++ test/test_single_number_ii.py
@@ -13,3 +13,13 @@ class TestSolution(TestCase):
         res = solution.singleNumber([0,1,0,1,0,1,99])
         self.assertEqual(99, res)
 
+    def test_singleNumber_1(self):
+        solution = Solution()
+        res = solution.singleNumber([-2,-2,1,1,4,1,4,4,-4,-2])
+        self.assertEqual(-4, res)
+
+    def test_singleNumber_2(self):
+        solution = Solution()
+        res = solution.singleNumber([2,2,2,-1,-1,-1,8,-7,0,-7,0,-7,0])
+        self.assertEqual(8, res)
+
