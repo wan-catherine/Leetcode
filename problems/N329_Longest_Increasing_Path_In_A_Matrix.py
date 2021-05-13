e12107c98b6445bd8786463728c7ae6b7f32482c
wanwan <rarry2012@gmail.com>
2021-05-12 22:10:06 -0400

    329

--- problems/N329_Longest_Increasing_Path_In_A_Matrix.py
+++ problems/N329_Longest_Increasing_Path_In_A_Matrix.py
@@ -1,4 +1,5 @@
 import collections
+from typing import List
 
 
 class Solution(object):
@@ -41,4 +42,30 @@ class Solution(object):
                     indegrees[i][j] -= 1
                     if not indegrees[i][j]:
                         zero_ingrees.append((i,j))
+        return res
+
+    """
+    here memo[(i,j)] mens the longest increasing path which starts from matrix[i][j]
+    """
+    def longestIncreasingPath_dfs(self, matrix: List[List[int]]) -> int:
+        rows, cols = len(matrix), len(matrix[0])
+        res = 0
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        memo = {}
+        def dfs(r, c):
+            if (r, c) in memo:
+                return memo[(r, c)]
+            ans = 1
+            for i, j in directions:
+                row, col = r + i, c + j
+                if row < 0 or row >= rows or col < 0 or col >= cols or matrix[row][col] <= matrix[r][c] :
+                    continue
+                ans = max(ans, dfs(row, col) + 1)
+            memo[(r,c)] = ans
+            return ans
+
+        for i in range(rows):
+            for j in range(cols):
+                ans = dfs(i, j)
+                res = max(res, ans)
         return res
