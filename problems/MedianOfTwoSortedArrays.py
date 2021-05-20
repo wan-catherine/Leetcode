385bf85b05c59f7f85e6de7dd37a2b4053803f44
wanwan <rarry2012@gmail.com>
2021-05-20 11:20:33 -0400

    4

--- problems/MedianOfTwoSortedArrays.py
+++ problems/MedianOfTwoSortedArrays.py
@@ -1,5 +1,9 @@
+import sys
+from typing import List
+
+
 class Solution:
-    def findMedianSortedArrays(self, nums1, nums2):
+    def findMedianSortedArrays_(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
@@ -32,3 +36,29 @@ class Solution:
             median = (res[index - 1] + res[index]) / 2
 
         return median
+
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            return self.findMedianSortedArrays(nums2, nums1)
+        l, r = 0, m + 1
+        while l < r:
+            pm = (r - l) // 2 + l
+            pn = (m + n + 1) // 2 - pm
+            maxlm = nums1[pm - 1] if pm > 0 else -sys.maxsize
+            minrm = nums1[pm] if pm < m else sys.maxsize
+
+            maxln = nums2[pn - 1] if pn > 0 else -sys.maxsize
+            minrn = nums2[pn] if pn < n else sys.maxsize
+
+            if maxlm <= minrn and maxln <= minrm:
+                if (m + n) % 2:
+                    return max(maxlm, maxln)
+                else:
+                    return (max(maxlm, maxln) + min(minrn, minrm)) / 2
+            elif maxlm > minrn:
+                r = pm
+            else:
+                l = pm + 1
+
+
