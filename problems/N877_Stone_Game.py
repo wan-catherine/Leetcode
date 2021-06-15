e380db4f5e2442402e502a2124d47209c7497a28
wan.wan <rarry1988528@126.com>
2021-06-15 16:40:14 -0400

    877

--- problems/N877_Stone_Game.py
+++ problems/N877_Stone_Game.py
@@ -2,8 +2,11 @@
 Minimax
 Same as 486. Predict the Winner: https://leetcode.com/problems/predict-the-winner/
 """
+from typing import List
+
+
 class Solution(object):
-    def stoneGame(self, piles):
+    def stoneGame_dfs(self, piles):
         """
         :type piles: List[int]
         :rtype: bool
@@ -25,3 +28,17 @@ class Solution(object):
 
         scores = dfs(0, len(piles)-1)
         return scores[0] > scores[1]
+
+    """
+    dp[i][j]: the maximum diff for piles[i:j+1] 
+    """
+    def stoneGame(self, piles: List[int]) -> bool:
+        length = len(piles)
+        dp = [[0] * length for _ in range(length)]
+        for i in range(1, length):
+            dp[i-1][i] = abs(piles[i-1] - piles[i])
+        for l in range(3, length+1):
+            for i in range(length - l + 1):
+                j = i + l - 1
+                dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1])
+        return dp[0][-1] > 0
