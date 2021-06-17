4df1e01c488850c5026e7ba6e57f6e1dd0e0ae5d
wan.wan <rarry1988528@126.com>
2021-06-17 10:28:00 -0400

    795

--- problems/N795_Numbers_Of_Subarrays_With_Bounded_Maximum.py
+++ problems/N795_Numbers_Of_Subarrays_With_Bounded_Maximum.py
@@ -10,8 +10,11 @@ Three situations:
     3. A[i] < L : so it can be follow a good subarray dp[i+1] = dp[i]
 
 """
+from typing import List
+
+
 class Solution(object):
-    def numSubarrayBoundedMax(self, A, L, R):
+    def numSubarrayBoundedMax_dp(self, A, L, R):
         """
         :type A: List[int]
         :type L: int
@@ -34,4 +37,31 @@ class Solution(object):
             else:
                 dp[i+1] = dp[i]
             res += dp[i+1]
+        return res
+
+    def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+        length = len(nums)
+        stack = []
+        res = 0
+        def helper(arr):
+            l = len(arr)
+            ans = (l + 1) * l // 2
+            n = 0
+            for i, c in enumerate(arr):
+                if c >= left:
+                    ans -= (n + 1) * n // 2
+                    n = 0
+                    continue
+                n += 1
+                if i == l - 1:
+                    ans -= (n + 1) * n // 2
+            return ans
+        for i in range(length):
+            if nums[i] > right:
+                res += helper(stack)
+                stack.clear()
+                continue
+            stack.append(nums[i])
+            if i == length - 1:
+                res += helper(stack)
         return res
