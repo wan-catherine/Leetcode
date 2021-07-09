66f6444dfc7c41d5511af9a6f56fd349b4c59189
wan.wan <rarry1988528@126.com>
2021-07-09 14:37:04 -0400

    981

--- problems/N981_Time_Based_Key_Value_Store.py
+++ problems/N981_Time_Based_Key_Value_Store.py
@@ -31,3 +31,28 @@ class TimeMap:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
+
+class TimeMap_1:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.tm = collections.defaultdict(dict)
+        self.times = []
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if timestamp not in self.tm:
+            bisect.insort_left(self.times, timestamp)
+        self.tm[timestamp][key] = value
+
+
+    def get(self, key: str, timestamp: int) -> str:
+        times = self.times
+        index = bisect.bisect(times, timestamp)
+        if index == 0 :
+            return ""
+        for i in range(index-1, -1, -1):
+            if key in self.tm[times[i]]:
+                return self.tm[times[i]][key]
+        return ""
