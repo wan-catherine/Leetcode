a17efd4879145ab934c1dadd5dda46d63b6d57ad
wan.wan <rarry1988528@126.com>
2021-08-02 17:34:02 -0400

    581

--- problems/N581_Shortest_Unsorted_Continuous_Subarray.py
+++ problems/N581_Shortest_Unsorted_Continuous_Subarray.py
@@ -1,5 +1,6 @@
 import bisect
 import sys
+from typing import List
 
 
 class Solution(object):
@@ -41,4 +42,42 @@ class Solution(object):
                 end = i
             stack.append(i)
         return end - start + 1 if start > -1 and end > -1 else 0
+
+    """
+    update : 202100802
+    steps: 
+        1. from left to right, find the first left index which a > b 
+        2. from right to left, find the first right index which a > b
+        3. find the maximum and minimum in range [start, end] (both included)
+        4. extend to left if there is any num > minimum of #3
+        5. extend to right if there is any num < maximum of #3
+    """
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        length = len(nums)
+        start = end = 0
+        for i in range(1, length):
+            if nums[i] >= nums[i - 1]:
+                continue
+            start = i - 1
+            break
+        for i in range(length - 2, -1, -1):
+            if nums[i] <= nums[i + 1]:
+                continue
+            end = i + 1
+            break
+        m, n = -sys.maxsize, sys.maxsize
+        for i in range(start, end + 1):
+            m = max(m, nums[i])
+            n = min(n, nums[i])
+        while start - 1 >= 0:
+            if nums[start - 1] > n:
+                start -= 1
+            else:
+                break
+        while end + 1 < length:
+            if nums[end + 1] < m:
+                end += 1
+            else:
+                break
+        return end - start + 1 if end > start else 0
 
