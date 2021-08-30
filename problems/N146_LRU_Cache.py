c6ad71e7d6e2c001d420ab0e4162fb37d9009bd2
wan.wan <rarry1988528@126.com>
2021-08-30 17:24:34 -0400

    146

--- problems/N146_LRU_Cache.py
+++ problems/N146_LRU_Cache.py
@@ -1,3 +1,4 @@
+import collections
 from collections import OrderedDict
 """
 Hashtable and DoubleList
@@ -8,7 +9,7 @@ node:
     next
 """
 
-class LRUCache(object):
+class LRUCache_orderedDict(object):
 
     def __init__(self, capacity):
         """
@@ -39,3 +40,57 @@ class LRUCache(object):
         else:
             self.cache.move_to_end(key)
         self.cache[key] = value
+
+"""
+We need ot maintain an order which the latest one is the at end . 
+But move an element in an array will be o(n), so we can consider double-linklist . 
+Use a dict to record all key-node.
+"""
+class Node:
+    def __init__(self, key, val, prev = None, nxt = None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.nxt = nxt
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.kv = collections.defaultdict(Node)
+        self.capacity = capacity
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
+        self.head.nxt = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.kv:
+            return -1
+        node = self.kv[key]
+        self.remove_node(node)
+        self.add_node(node)
+        return node.val
+
+    def remove_node(self, node):
+        prev = node.prev
+        nxt = node.nxt
+        prev.nxt = nxt
+        nxt.prev = prev
+
+    def add_node(self, node):
+        prev = self.tail.prev
+        prev.nxt = node
+        node.prev = prev
+        node.nxt = self.tail
+        self.tail.prev = node
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.kv:
+            node = self.kv[key]
+            self.remove_node(node)
+        elif len(self.kv) == self.capacity:
+            del self.kv[self.head.nxt.key]
+            self.remove_node(self.head.nxt)
+        node = Node(key, value)
+        self.kv[key] = node
+        self.add_node(node)
