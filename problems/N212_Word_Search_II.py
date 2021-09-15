472d24f5e379e87b9c445ad7c98c15c6c64f227c
wan.wan <rarry1988528@126.com>
2021-09-15 16:13:32 -0400

    212

--- problems/N212_Word_Search_II.py
+++ problems/N212_Word_Search_II.py
@@ -1,3 +1,7 @@
+import collections
+from typing import List
+
+
 class TrieNode:
     def __init__(self):
         self.children = [None] * 26
@@ -100,5 +104,52 @@ class Solution(object):
 
         self.board[row][col] = temp
 
+    # DFS + prune
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        rows, cols = len(board), len(board[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        count = collections.defaultdict(int)
+        for i in range(rows):
+            for j in range(cols):
+                count[board[i][j]] += 1
 
+        def check(word):
+            def dfs(r, c, index):
+                if board[r][c] != word[index]:
+                    return False
+                if index == len(word) - 1:
+                    return True
+                visited[r][c] = 1
+                for i, j in directions:
+                    row, col = r + i, c + j
+                    if row < 0 or row >= rows or col < 0 or col >= cols or visited[row][col]:
+                        continue
+                    if board[row][col] != word[index + 1]:
+                        continue
+                    visited[row][col] = 1
+                    if dfs(row, col, index + 1):
+                        return True
+                    visited[row][col] = 0
+                return False
+
+            for i in range(rows):
+                for j in range(cols):
+                    visited = [[0] * cols for _ in range(rows)]
+                    if dfs(i, j, 0):
+                        return True
+            return False
+
+        res = []
+        for word in words:
+            if len(word) > rows * cols:
+                continue
+            cw = collections.Counter(word)
+            flag = True
+            for k, v in cw.items():
+                if k not in count or count[k] < v:
+                    flag = False
+                    break
+            if flag and check(word):
+                res.append(word)
+        return res
 
