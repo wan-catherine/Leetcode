c0ea4be26758cf2d263f985966bc59fc0ea038ed
wan.catherine <rarry1988528@126.com>
2021-10-09 15:53:04 -0400

    907

--- problems/N907_Sum_Of_Subarray_Minimus.py
+++ problems/N907_Sum_Of_Subarray_Minimus.py
@@ -1,3 +1,7 @@
+import sys
+from typing import List
+
+
 class Solution(object):
     # time complexsity is o(n**2), timeout
     def sumSubarrayMins_timeout(self, A):
@@ -16,7 +20,7 @@ class Solution(object):
                 res += min
         return res % mod
 
-    # explaination : https://leetcode.com/problems/sum-of-subarray-minimums/discuss/178876/stack-solution-with-very-detailed-explanation-step-by-step
+    # explanation : https://leetcode.com/problems/sum-of-subarray-minimums/discuss/178876/stack-solution-with-very-detailed-explanation-step-by-step
     # for one number a, all sub-arrays which the minute is a.
     # left[a] means the count of numbers which on the left of a and all >= a
     # right[a] means the count of numbers which on the right of a and all > a
@@ -67,3 +71,27 @@ class Solution(object):
                 res += A[j] * (i - j) * (j - k)
             s.append(i)
         return res % (10 ** 9 + 7)
+
+    # update at 20211008
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        length = len(arr)
+        left, right = [sys.maxsize] * length, [sys.maxsize] * length
+        stack = []
+        for i in range(length):
+            count = 1
+            while stack and stack[-1][0] >= arr[i]:
+                count += stack.pop()[1]
+            left[i] = count
+            stack.append((arr[i], count))
+        stack = []
+        for i in range(length - 1, -1, -1):
+            count = 1
+            while stack and stack[-1][0] > arr[i]:
+                count += stack.pop()[1]
+            right[i] = count
+            stack.append((arr[i], count))
+        res = 0
+        for i in range(length):
+            res += arr[i] * left[i] * right[i]
+            res %= (10 ** 9 + 7)
+        return res
