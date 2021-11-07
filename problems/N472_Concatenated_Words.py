ea018a39a429bcf0cf479a93f0e62a89de6a1429
wan.catherine <rarry1988528@126.com>
2021-11-07 12:04:36 -0500

    472

--- problems/N472_Concatenated_Words.py
+++ problems/N472_Concatenated_Words.py
@@ -41,3 +41,40 @@ class Solution:
                 cur = cur.children[index]
             cur.isend = True
         return res
+
+    # update at 20111107
+    def findAllConcatenatedWordsInADict_after(self, words: List[str]) -> List[str]:
+        words.sort(key=lambda x: len(x))
+        root = Trie()
+
+        def check(index, word):
+            if index == len(word):
+                return True
+            if (index, word) in memo:
+                return memo[(index, word)]
+            cur = root
+            flag = False
+            for i in range(index, len(word)):
+                idx = ord(word[i]) - ord('a')
+                if not cur.children[idx]:
+                    break
+                cur = cur.children[idx]
+                if cur.end and check(i + 1, word):
+                    flag = True
+            memo[(index, word)] = flag
+            return flag
+
+        res = []
+        memo = {}
+        for word in words:
+
+            if word and check(0, word):
+                res.append(word)
+            cur = root
+            for c in word:
+                idx = ord(c) - ord('a')
+                if not cur.children[idx]:
+                    cur.children[idx] = Trie()
+                cur = cur.children[idx]
+            cur.end = True
+        return res
