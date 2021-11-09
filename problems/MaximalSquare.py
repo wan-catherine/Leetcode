501e214a283c5d9051fb1322e310029c577cb9cd
wan.catherine <rarry1988528@126.com>
2021-11-09 14:41:40 -0500

    221

--- problems/MaximalSquare.py
+++ problems/MaximalSquare.py
@@ -1,3 +1,5 @@
+from typing import List
+
 
 class Solution:
     def maximalSquare(self, matrix):
@@ -24,9 +26,35 @@ class Solution:
                     temp = min(cache[i-1][j], cache[i][j-1], cache[i-1][j-1])
                     cache[i][j] = int(matrix[i][j]) + temp
                 if cache[i][j] > max:
+
                     max = cache[i][j]
+                    print(max, i, j)
 
         return max * max
+
+    """
+    Similar as leetcode 85. 
+    Here we use non-decreasing stack. 
+    each time when we pop out , it's the lowest number amount [index:cur]
+    """
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        rows, cols = len(matrix), len(matrix[0])
+        arr = [0] * (cols + 2)
+        res = 0
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == "1":
+                    arr[c + 1] += 1
+                else:
+                    arr[c + 1] = 0
+            stack = []
+            for i in range(cols + 2):
+                while stack and arr[stack[-1]] > arr[i]:
+                    index = stack.pop()
+                    res = max(res, min(arr[index], i - stack[-1] - 1))
+                stack.append(i)
+        return res ** 2
+
 
 
 
