8cf17cf8e32ed1fd65b9b6df3ea0b4df1c355a7a
wan.catherine <rarry1988528@126.com>
2021-11-10 20:45:35 -0500

    sort

--- problems/SortColors.py
+++ problems/SortColors.py
@@ -14,7 +14,6 @@ class Solution:
                 i += 1
             elif nums[i] < 1:
                 nums[i],nums[left] = nums[left], nums[i]
-                i += 1
                 left += 1
             else:
                 nums[i], nums[right] = nums[right],nums[i]
