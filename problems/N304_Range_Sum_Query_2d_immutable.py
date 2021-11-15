2044df952a1b1c7351d45d7edada02f9d1bb9f28
wan.catherine <rarry1988528@126.com>
2021-11-15 11:40:40 -0500

    304

--- problems/N304_Range_Sum_Query_2d_immutable.py
+++ problems/N304_Range_Sum_Query_2d_immutable.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class NumMatrix(object):
 
     def __init__(self, matrix):
@@ -42,6 +45,19 @@ class NumMatrix(object):
             res += self.sum_matrix[row_overlap][col_overlap]
         return res
 
+class NumMatrix:
+    def __init__(self, matrix: List[List[int]]):
+        rows, cols = len(matrix), len(matrix[0])
+        self.grid = [[0] * (cols + 1) for _ in range(rows + 1)]
+        for i in range(1, rows + 1):
+            for j in range(1, cols + 1):
+                self.grid[i][j] = self.grid[i-1][j] + self.grid[i][j-1] - self.grid[i-1][j-1] + matrix[i-1][j-1]
+        for row in self.grid:
+            print(row)
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.grid[row2 + 1][col2 + 1] - self.grid[row1][col2 + 1] - self.grid[row2 + 1][col1] + self.grid[row1][col1]
+
 # Your NumMatrix object will be instantiated and called as such:
 matrix = [
   [3, 0, 1, 4, 2],
@@ -50,7 +66,7 @@ matrix = [
   [4, 1, 0, 1, 7],
   [1, 0, 3, 0, 5]
 ]
-matrix = [[]]
+# matrix = [[]]
 obj = NumMatrix(matrix)
 param_1 = obj.sumRegion(2, 1, 4, 3)
 print(param_1)
