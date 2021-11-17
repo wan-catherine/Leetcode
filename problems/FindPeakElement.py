b8bf365fd6d5d609ab2349dbe86b13366cade620
wan.catherine <rarry1988528@126.com>
2021-11-17 10:48:07 -0500

    162

--- problems/FindPeakElement.py
+++ problems/FindPeakElement.py
@@ -38,3 +38,26 @@ class Solution(object):
                 r = mid
             elif mid < length - 1 and nums[mid] < nums[mid+1]:
                 l = mid + 1
+
+    def findPeakElement(self, nums: List[int]) -> int:
+        length = len(nums)
+        if length == 1:
+            return 0
+        if nums[0] > nums[1]:
+            return 0
+        if nums[-1] > nums[-2]:
+            return length - 1
+        l, r = 0, length
+        while l < r:
+            mid = (r - l) // 2 + l
+            if mid + 1 < length:
+                if nums[mid] < nums[mid + 1]:
+                    l = mid + 1
+                else:
+                    r = mid
+            elif mid - 1 >= 0:
+                if nums[mid] < nums[mid - 1]:
+                    r = mid
+                else:
+                    l = mid - 1
+        return l
