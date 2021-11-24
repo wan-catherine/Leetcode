b643632009752d937b7cd01cab50714595d3a66b
wan.catherine <rarry1988528@126.com>
2021-11-24 18:42:38 -0500

    273

--- problems/N273_Integer_To_English_Words.py
+++ problems/N273_Integer_To_English_Words.py
@@ -23,3 +23,46 @@ class Solution:
                 res = helper(n//10**9) + " Billion " + helper(n%10**9)
             return res.strip()
         return helper(num)
+
+    def numberToWords(self, num: int) -> str:
+        twenties = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+                    "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        hundreds = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        bigs = ["Billion", "Million", "Thousand", ""]
+
+        def helper(n):
+            res = ''
+            flag = False
+            if n >= 100:
+                res += twenties[n // 100] + ' Hundred'
+                n = n % 100
+                flag = True
+            if n == 0:
+                return res
+            res += ' ' if flag else ''
+            flag = False
+            if n >= 20:
+                res += hundreds[n // 10]
+                n = n % 10
+                flag = True
+            if n == 0:
+                return res
+            res += ' ' if flag else ''
+            if n > 0:
+                res += twenties[n]
+            return res
+
+        if num == 0:
+            return 'Zero'
+        i = 10 ** 9
+        res = ''
+        j = 0
+        while i > 0:
+            if num >= i:
+                res += helper(num // i)
+                num = num % i
+                res += " " + bigs[j] + " "
+            j += 1
+            i //= 10 ** 3
+            # print(num, i )
+        return res.strip()
