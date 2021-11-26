612e67a396df0f63672118a63486d7f2d45c5b05
wan.catherine <rarry1988528@126.com>
2021-11-26 16:39:15 -0500

    918

--- problems/N918_Maximum_Sum_Circular_Subarrary.py
+++ problems/N918_Maximum_Sum_Circular_Subarrary.py
@@ -24,7 +24,7 @@ Then we can see it : d[i]=max(A[i] , d[i-1]+A[i])
 
 second about the circular subarray :
     there are two situations:
-        1. maximum subarray is in the array which is the normal Kadane Algrorith
+        1. maximum subarray is in the array which is the normal Kadane Algorithm
         2. maximum subarray is in teh circular one :
             we can find the minimum subarray of the array,
             then sum(A) - minimum subarray value = maximum
@@ -32,6 +32,8 @@ second about the circular subarray :
         sum(A) == minimum subarray value
     at this time, return maximum subarray value which should be the minimum single value in the array
 """
+from typing import List
+
 
 class Solution:
     def maxSubarraySumCircular(self, A) -> int:
@@ -50,4 +52,23 @@ class Solution:
             max_value = max(current_value, max_value)
         return max_value
 
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        has_positive = False
+        for n in nums:
+            if n > 0:
+                has_positive = True
+                break
+        if not has_positive:
+            return max(nums)
 
+        total = sum(nums)
+        length = len(nums)
+        dpmax = [0] * length
+        dpmin = [0] * length
+        dpmax[0] = dpmin[0] = nums[0]
+        res = nums[0]
+        for i in range(1, length):
+            dpmax[i] = max(dpmax[i - 1], 0) + nums[i]
+            dpmin[i] = min(dpmin[i - 1], 0) + nums[i]
+            res = max(res, dpmax[i], total - dpmin[i])
+        return res
