968f208b0d6d5a3aae02e95a25ae411e6673e6ed
wan.catherine <rarry1988528@126.com>
2021-11-29 09:01:02 -0500

    518

--- problems/N518_Coin_Change_2.py
+++ problems/N518_Coin_Change_2.py
@@ -15,16 +15,25 @@ class Solution:
             db_table.append(row)
         return db_table[-1][-1]
 
-    # d[i][j] : by using the first i coins, the totally amount is j, then it means the number
-    # of coins that make up that amount j
-    # d[i][j] seperate two situations :
-    #         1. not used the ith coin : d[i-1][j]
-    #         2. used the ith coin : d[i][j - coins[i-1]]
-    # d[i][j] = d[i-1][j] + d[i][j-coins[i-1]]
-    # Here because we can reuse the number in coins, so for the second situation
-    # it will be d[i], because the coins there is infinite
-    # also need to check if j - coins[i-1] >= 0
-
+    """
+    Knapsack: 
+    
+    d[i][j] : by using the first i coins, the totally amount is j, then it means the number
+    of coins that make up that amount j
+    dp[i][j] separate two situations :
+            1. not used the ith coin : dp[i-1][j]
+            2. used the ith coin k times (k * coins[i-1] <= j) :  c = coins[i-1]                
+                dp[i-1][j - c] + dp[i-1][j - 2*c] + dp[i-1][j - 3*c] + ... + dp[i-1][j - k*c] 
+                
+    so from #1 and # 2 , we can know : 
+        dp[i][j] = dp[i-1][j] + dp[i-1][j - c] + dp[i-1][j - 2*c] + dp[i-1][j - 3*c] + ... + dp[i-1][j - k*c] 
+    here we can do optimize:
+        dp[i][j-c] =            dp[i-1][j - c] + dp[i-1][j - 2*c] + dp[i-1][j - 3*c] + ... + dp[i-1][j - k*c]
+        
+    ==> dp[i][j] = dp[i-1][j] + dp[i][j-c] 
+    ==> dp[j] = dp[j] + dp[j-c]
+    ==> dp[j] += dp[j-c]
+    """
     def change(self, amount, coins):
         dp = [1] + [0] * amount
         for i in range(1, len(coins) +1):
