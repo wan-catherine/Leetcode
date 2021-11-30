d8f5c41858febd35fcf9ef21b549bf7fa90fa13f
wan.catherine <rarry1988528@126.com>
2021-11-30 10:59:54 -0500

    221

--- problems/MaximalSquare.py
+++ problems/MaximalSquare.py
@@ -55,7 +55,30 @@ class Solution:
                 stack.append(i)
         return res ** 2
 
-
+    "DP"
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        rows, cols = len(matrix), len(matrix[0])
+        dp = [[0] * cols for _ in range(rows)]
+        res = 0
+        for i in range(rows):
+            if matrix[i][0] == '1':
+                dp[i][0] = 1
+                res = 1
+        for i in range(cols):
+            if matrix[0][i] == '1':
+                dp[0][i] = 1
+                res = 1
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] == "0":
+                    continue
+                val = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+                if val == 0:
+                    dp[i][j] = 1 if matrix[i][j] == '1' else 0
+                else:
+                    dp[i][j] = val + 1
+                res = max(res, dp[i][j])
+        return res * res
 
 
 
