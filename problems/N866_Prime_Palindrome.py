11fa87fd5457cd6a00eba58b0cba0cdb4faeddd0
wan.catherine <rarry1988528@126.com>
2021-12-02 18:58:47 -0500

    866

--- problems/N866_Prime_Palindrome.py
+++ problems/N866_Prime_Palindrome.py
@@ -2,7 +2,7 @@ import math
 
 
 class Solution:
-    def primePalindrome(self, n: int) -> int:
+    def primePalindrome_before(self, n: int) -> int:
         length = len(str(n))
 
         def is_prime(num):
@@ -48,7 +48,42 @@ class Solution:
                         return num
             length += 1
 
+    """
+    Update at 20211202
+    Key point : xyzzyx will not be prime, because it can be divided by 11, except 11 itself.
+    So we need to deal with the special situation when n <= 11. 
+    
+    Then for all other palindrome, we can only consider xyz ==> xyzyx. 
+    
+    """
+    def primePalindrome(self, n: int) -> int:
+        if n <= 2:
+            return 2
+        if n <= 3:
+            return 3
+        if n <= 5:
+            return 5
+        if n <= 7:
+            return 7
+        if n <= 11:
+            return 11
+        ns = str(n)
+        ln = len(ns)
 
+        def check(num):
+            for i in range(2, int(math.sqrt(num)) + 1):
+                if num % i == 0:
+                    return False
+            return True
 
+        num = 10 ** (ln // 2)
+        while True:
+            half = str(num)
+            val = int(half + half[:-1][::-1])
+
+            # print(val)
+            if val >= n and check(val):
+                return val
+            num += 1
 
 
