319cf38a9898f56fd2b35674c3f1ebc5d3c39ed2
wan.catherine <rarry1988528@126.com>
2022-07-05 13:04:42 -0400

    174

--- problems/N174_Dungeon_Game.py
+++ problems/N174_Dungeon_Game.py
@@ -16,7 +16,7 @@ class Solution(object):
         col = len(dungeon[0])+1
         dp_table = [[float(inf)]*col for _ in range(row)]
         dp_table[-1][-2], dp_table[-2][-1] = 1,1
-        for i in range(row-2,-1,-1):
-            for j in range(col-2,-1,-1):
+        for i in range(row-1,-1,-1):
+            for j in range(col-1,-1,-1):
                 dp_table[i][j] = max(1, min(dp_table[i+1][j], dp_table[i][j+1]) - dungeon[i][j])
         return dp_table[0][0]
