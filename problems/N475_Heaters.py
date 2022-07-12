d8dc131a4670475e0f00bd2831d52185c580bf7d
wan.catherine <rarry1988528@126.com>
2022-07-12 16:12:53 -0400

    475

--- problems/N475_Heaters.py
+++ problems/N475_Heaters.py
@@ -1,4 +1,5 @@
 import bisect
+from typing import List
 
 
 class Solution(object):
@@ -21,4 +22,32 @@ class Solution(object):
                 dist = min(h-heaters[index-1], heaters[index]-h)
             res = max(res, dist)
         return res
+
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        lh, lt = len(houses), len(heaters)
+        houses.sort()
+        heaters.sort()
+
+        def check(m):
+            covered = [0] * lh
+            i, j = 0, 0
+            while i < lh and j < lt:
+                if covered[i]:
+                    i += 1
+                    continue
+                if abs(heaters[j] - houses[i]) <= m:
+                    covered[i] = 1
+                    i += 1
+                else:
+                    j += 1
+            return sum(covered) == lh
+
+        l, r = 0, 10 ** 9
+        while l < r:
+            mid = (r - l) // 2 + l
+            if check(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
 
