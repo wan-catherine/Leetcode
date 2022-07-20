d02ccd82f7b0514ea1ec4115172a1aaa1c91d726
wan.catherine <rarry1988528@126.com>
2022-07-20 18:26:18 -0400

    3334

--- problems/N334_Increasing_Triplet_Subsequence.py
+++ problems/N334_Increasing_Triplet_Subsequence.py
@@ -2,6 +2,10 @@
 Notice here one and two can be zero.
 So we can't use 'not one/two' to check if it's None
 """
+import sys
+from typing import List
+
+
 class Solution(object):
     def increasingTriplet(self, nums):
         """
@@ -22,5 +26,19 @@ class Solution(object):
                 continue
             if one > i:
                 one = i
+        return False
+
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        length = len(nums)
+        if length < 3:
+            return False
+        one, two = sys.maxsize, sys.maxsize
+        for i in range(length):
+            if nums[i] > two:
+                return True
+            if nums[i] > one:
+                two = nums[i]
+            else:
+                one = nums[i]
         return False
 
