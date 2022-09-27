08017933fb09ca3d3b1d6ca6ad14687f18ccf507
wan.catherine <rarry1988528@126.com>
2022-09-27 15:07:17 -0400

    698

--- problems/N698_Partition_To_K_Equal_Sum_Subsets.py
+++ problems/N698_Partition_To_K_Equal_Sum_Subsets.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution(object):
     def canPartitionKSubsets_dfs(self, nums, k):
         """
@@ -30,7 +33,7 @@ class Solution(object):
             self.visited[i] = 0
         return False
 
-    def canPartitionKSubsets(self, nums, k):
+    def canPartitionKSubsets_tle(self, nums, k):
         sum_nums = sum(nums)
         if sum_nums % k:
             return False
@@ -48,6 +51,44 @@ class Solution(object):
                     targets[i] += nums[index]
             return False
         return dfs(0)
+
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total = sum(nums)
+        if total % k:
+            return False
+        val = total // k
+
+        nums.sort(reverse=True)
+        length = len(nums)
+        visited = [0] * length
+
+        memo = set()
+
+        def dfs(cur, t):
+            if (cur, t, tuple(visited)) in memo:
+                return False
+            if cur == 0 and t == k:
+                return True
+            if cur > val:
+                return False
+
+            for i in range(length):
+                if visited[i]:
+                    continue
+                if cur + nums[i] > val:
+                    continue
+                visited[i] = 1
+                if cur + nums[i] == val:
+                    if dfs(0, t + 1):
+                        return True
+                else:
+                    if dfs(cur + nums[i], t):
+                        return True
+                visited[i] = 0
+            memo.add((cur, t, tuple(visited)))
+            return False
+
+        return dfs(0, 0)
 
 
 
