fc92de04eadc56d33d33c10c27af6e5113832abe
wan.catherine <rarry1988528@126.com>
2022-10-11 17:46:50 -0400

    327

--- problems/N327_Count_Of_Range_Sum.py
+++ problems/N327_Count_Of_Range_Sum.py
@@ -1,7 +1,6 @@
 """
-Too compliated .
+Too complicated .
 Use merge sort
-
 """
 import bisect
 from bisect import bisect_left, bisect_right, insort_left
@@ -72,20 +71,21 @@ class Solution(object):
             rgs += rpos - lpos
             insort_left(sums, -base)
         return rgs
-
-    # use the same idea as #560
-    # in order to find sum[i:j] in [lower, upper]
-    # sum[0:j] - sum[0:i] in [lower, upper]
-    # binary search to find the position for sum[0:j] - lower and sum[0:j] - upper
-    # lower <= upper , so x - lower >= x - upper which means if insert they are in prefix_sum array ,
-    # the position(x-lower) > positon(x-upper)
-    # in order to deal with the dupliated numer in the prefix_sum array ,
-    # we need to find the right-est positon for x-lower and find the left-est position for x-upper
-    # there are several situations
-    # y=bisect.bisect(prefix_sum, temp_lower) - bisect.bisect_left(prefix_sum, temp_upper)
-    # first : position for both of them are in the middle of the prefix_sum, then y will the count
-    # second: x-lower>=x-upper > prefix_sum[-1], then y =0
-    # third: prefix_sum[0] > x-lower>= x-upper, then y=0
+    """
+    use the same idea as #560
+    in order to find sum[i:j] in [lower, upper]
+    sum[0:j] - sum[0:i] in [lower, upper]
+    binary search to find the position for sum[0:j] - lower and sum[0:j] - upper
+    lower <= upper , so x - lower >= x - upper which means if insert they are in prefix_sum array ,
+    the position(x-lower) > position(x-upper)
+    in order to deal with the duplicated number in the prefix_sum array ,
+    we need to find the right-est position for x-lower and find the left-est position for x-upper
+    there are several situations
+    y=bisect.bisect(prefix_sum, temp_lower) - bisect.bisect_left(prefix_sum, temp_upper)
+        first : position for both of them are in the middle of the prefix_sum, then y will the count
+        second: x-lower>=x-upper > prefix_sum[-1], then y=0
+        third: prefix_sum[0] > x-lower>= x-upper, then y=0
+    """
     def countRangeSum(self, nums, lower, upper):
         sum = 0
         prefix_sum = [0]
