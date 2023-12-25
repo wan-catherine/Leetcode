a32391fd05e00cf5b6263e99b7eb759d0c9670d6
wan.catherine <rarry1988528@126.com>
2023-12-25 11:26:58 -0500

    Update N2954_Count_The_Number_Of_Infection_Sequences.py

--- problems/N2954_Count_The_Number_Of_Infection_Sequences.py
+++ problems/N2954_Count_The_Number_Of_Infection_Sequences.py
@@ -2,9 +2,18 @@ from functools import cache
 from itertools import pairwise
 from typing import List
 
+"""
+ret = n!;
+for (int i=0; i<k; i++)
+  ret = ret / mi! * (2^mi);
+  
+(A*B)%M = (A%M) * (B%M), 
+but for "/", 
+inv(a) = a ^ (M-2) % M
+"""
 
 class Solution:
-    def numberOfSequence(self, n: int, sick: List[int]) -> int:
+    def numberOfSequence_(self, n: int, sick: List[int]) -> int:
         MOD = 10 ** 9 + 7
 
         @cache
