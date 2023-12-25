cd676dc68c61ad9475456ce7cb603333edb9c3da
wan.catherine <rarry1988528@126.com>
2023-12-25 11:57:46 -0500

    Update N2954_Count_The_Number_Of_Infection_Sequences.py

--- problems/N2954_Count_The_Number_Of_Infection_Sequences.py
+++ problems/N2954_Count_The_Number_Of_Infection_Sequences.py
@@ -13,7 +13,7 @@ inv(a) = a ^ (M-2) % M
 """
 
 class Solution:
-    def numberOfSequence_(self, n: int, sick: List[int]) -> int:
+    def numberOfSequence_MLE(self, n: int, sick: List[int]) -> int:
         MOD = 10 ** 9 + 7
 
         @cache
@@ -29,3 +29,41 @@ class Solution:
             res = res * ifact(group) % MOD
             res = res * pow(2, group - 1, MOD) % MOD
         return res
+
+    def numberOfSequence(self, n: int, sick: List[int]) -> int:
+        MOD = 10 ** 9 + 7
+        group = []
+        length = len(sick)
+        for i in range(length):
+            if i == 0:
+                group.append(sick[i])
+            else:
+                group.append(sick[i] - sick[i-1] - 1)
+        group.append(n - sick[-1] - 1)
+        fact = [1] * (n + 1)
+        for i in range(1, n + 1):
+            fact[i] = i * fact[i-1]
+        def quickPow(x, y):
+            res, cur = 1, x
+            while y > 0:
+                if y & 1:
+                    res = res * cur % MOD
+                cur = cur * cur % MOD
+                y >>= 1
+            return res
+
+        def inv(x):
+            return quickPow(x, MOD - 2)
+
+        res = fact[length]
+        for g in group:
+            res = res * inv(fact[g]) % MOD
+
+        for i in range(1, length):
+            x = sick[i] - sick[i-1] - 1
+            if x > 0:
+                res = res * pow(2, x, MOD) % MOD
+
+        return res
+
+
