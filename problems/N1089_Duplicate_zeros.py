642e9734ca2b9cf6efd97a24edb8931c3fd0c6b9
wan.catherine <rarry1988528@126.com>
2024-01-27 09:10:55 -0500

    1089

--- problems/N1089_Duplicate_zeros.py
+++ problems/N1089_Duplicate_zeros.py
@@ -1,4 +1,5 @@
 from collections import deque
+from typing import List
 
 
 class Solution(object):
@@ -35,3 +36,34 @@ class Solution(object):
                 arr[i] = q.popleft()
                 if arr[i] == 0:
                     isZero = True
+
+    '''
+    original arr:
+    [1, 0, 2, 0, 3, 0]
+    num of zeros so far:
+    [0, 1, 1, 2, 2, 3]
+    arr with duplicate 0s (Θ means a duplicate 0):
+    [1, Θ, 0, 2, Θ, 0, 3, Θ, 0]
+    arr after cutoff
+    [1, Θ, 0, 2, Θ, 0]
+    
+    # Every number in the original array is shifted to the right by the number of zeros so far.
+    # Because of the cutoff, we are going to check if the new position of the number is inside the array.
+    if i + zeroes < n:
+    
+    # This is checking if there is a Θ that should be included.
+    if arr[i] == 0:
+      zeroes -= 1
+      if i + zeroes < n:
+         arr[i + zeroes] = 0
+    '''
+    def duplicateZeros_2024(self, arr: List[int]) -> None:
+        zeroes = arr.count(0)
+        n = len(arr)
+        for i in range(n - 1, -1, -1):
+            if i + zeroes < n:
+                arr[i + zeroes] = arr[i]
+            if arr[i] == 0:
+                zeroes -= 1
+                if i + zeroes < n:
+                    arr[i + zeroes] = 0
