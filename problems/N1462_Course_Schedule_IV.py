fd51f96a11fbef704eabedf518d7deb9016563a9
wan.catherine <rarry1988528@126.com>
2025-01-27 18:20:00 -0500

    1462

--- problems/N1462_Course_Schedule_IV.py
+++ problems/N1462_Course_Schedule_IV.py
@@ -1,4 +1,5 @@
 import collections
+from typing import List
 
 
 class Solution(object):
@@ -34,6 +35,35 @@ class Solution(object):
         res = []
         for i, j in queries:
             if j in memo[i]:
+                res.append(True)
+            else:
+                res.append(False)
+        return res
+
+    def checkIfPrerequisite_20250127(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        dp = {}
+        graph = collections.defaultdict(list)
+        indegree = [0] * numCourses
+        for u, v in prerequisites:
+            indegree[v] += 1
+            graph[u].append(v)
+        def dfs(idx):
+            if idx in dp:
+                return dp[idx]
+            depends = set()
+            for nxt in graph[idx]:
+                depends.add(nxt)
+                depends.update(dfs(nxt))
+            dp[idx] = depends
+            return depends
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                dfs(i)
+
+        res = []
+        for u, v in queries:
+            if u in dp and v in dp[u]:
                 res.append(True)
             else:
                 res.append(False)
