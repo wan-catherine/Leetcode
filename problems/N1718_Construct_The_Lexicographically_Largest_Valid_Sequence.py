cfdb92cd8475b7f6715a7380196d3ec4cbffc1ce
wan.catherine <rarry1988528@126.com>
2025-02-16 11:27:12 -0500

    1718

--- problems/N1718_Construct_The_Lexicographically_Largest_Valid_Sequence.py
+++ problems/N1718_Construct_The_Lexicographically_Largest_Valid_Sequence.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def constructDistancedSequence(self, n: int):
         res = [0] * n*2
@@ -39,3 +42,38 @@ class Solution:
 
         dfs(1)
         return res[1:]
+
+    def constructDistancedSequence(self, n: int) -> List[int]:
+        used = [0] * (n + 1)
+        res = [0] * (2 * n - 1)
+
+        def dfs(index):
+            if index == 2 * n - 1:
+                return True
+
+            if res[index] != 0:
+                return dfs(index + 1)
+
+            for i in range(n, 0, -1):
+                if used[i] == 1:
+                    continue
+                if i > 1 and ((index + i) >= 2 * n - 1 or res[index + i]) != 0:
+                    continue
+
+                used[i] = 1
+                res[index] = i
+                if i > 1:
+                    res[index + i] = i
+
+                if dfs(index + 1):
+                    return True
+
+                used[i] = 0
+                res[index] = 0
+                if i > 1:
+                    res[index + i] = 0
+            return False
+
+        dfs(0)
+        return res
+
