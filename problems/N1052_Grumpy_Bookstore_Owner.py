c8174cf17c36b4dfbadb0e104bdc59440c66ce9c
wan.catherine <rarry1988528@126.com>
2025-02-17 10:29:42 -0500

    1052

--- problems/N1052_Grumpy_Bookstore_Owner.py
+++ problems/N1052_Grumpy_Bookstore_Owner.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution(object):
     def maxSatisfied_first_slow(self, customers, grumpy, X):
         """
@@ -43,3 +46,24 @@ class Solution(object):
             end += 1
             res = max(res, temp)
         return res + before
+
+    def maxSatisfied_20250217(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        length = len(customers)
+        prefix = customers[:]
+        for i in range(1, length):
+            prefix[i] += prefix[i-1]
+        prefix = [0] + prefix
+        ppre = [0]
+        for i in range(length):
+            if grumpy[i] == 0:
+                ppre.append(ppre[-1] + customers[i])
+            else:
+                ppre.append(ppre[-1])
+
+        res, j = 0, 0
+        for i in range(length):
+            j = min(i + minutes - 1, length - 1)
+            val = ppre[-1] - (ppre[j+1] - ppre[i]) + prefix[j+1] - prefix[i]
+            res = max(res, val)
+
+        return res
