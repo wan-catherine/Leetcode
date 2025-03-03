9c267faf1f792e7eaf5e7a260c9b7899100ad9fb
wan.catherine <rarry1988528@126.com>
2025-03-03 11:51:22 -0500

    2411

--- problems/N2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.py
+++ problems/N2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def smallestSubarrays(self, nums: List[int]) -> List[int]:
+    def smallestSubarrays_before(self, nums: List[int]) -> List[int]:
         ones = [0] * 30
         res, length = [], len(nums)
         for idx in range(length-1, -1, -1):
@@ -12,3 +12,24 @@ class Solution:
             res.append(max(1, max(ones) - idx + 1))
         return res[::-1]
 
+    def smallestSubarrays(self, nums: List[int]) -> List[int]:
+        ones = [0] * 32
+        res, length = [], len(nums)
+        j = length - 1
+        def check(j):
+            for i in range(32):
+                if ones[i] == 1 and nums[j] & (1 << i):
+                    return False
+            return True
+
+        for idx in range(length - 1, -1, -1):
+            for i in range(32):
+                if nums[idx] & (1 << i):
+                    ones[i] += 1
+            while j > idx and check(j):
+                for i in range(32):
+                    if nums[j] & (1 << i):
+                        ones[i] -= 1
+                j -= 1
+            res.append(j - idx + 1)
+        return res[::-1]
--- test/test_N2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.py
+++ test/test_N2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
 from problems.N2411_Smallest_Subarrays_With_Maximum_Bitwise_OR import Solution
 
+
 class TestSolution(TestCase):
     def test_smallest_subarrays(self):
-        self.assertListEqual([3,3,2,2,1], Solution().smallestSubarrays([1,0,2,1,3]))
+        self.assertListEqual([3, 3, 2, 2, 1], Solution().smallestSubarrays([1, 0, 2, 1, 3]))
 
     def test_smallest_subarrays_1(self):
-        self.assertListEqual([2,1], Solution().smallestSubarrays([1,2]))
+        self.assertListEqual([2, 1], Solution().smallestSubarrays([1, 2]))
+
