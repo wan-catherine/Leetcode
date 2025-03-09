9a14210a0d6065f6b1bc81d6fa94b7ce44abab7b
wan.catherine <rarry1988528@126.com>
2025-03-09 11:22:20 -0400

    2747

--- problems/N2747_Count_Zero_Request_Servers.py
+++ problems/N2747_Count_Zero_Request_Servers.py
@@ -31,5 +31,27 @@ class Solution:
             arr[idx] = n - len(count.keys())
         return arr
 
+    def countServers_20250309(self, n: int, logs: List[List[int]], x: int, queries: List[int]) -> List[int]:
+        lq = len(queries)
+        que = [(queries[i], i) for i in range(lq)]
+        que.sort(key=lambda x: x[0])
+        logs.sort(key=lambda x: x[1])
+        pre = collections.defaultdict(int)
+        ll = len(logs)
+        res = [0] * lq
+        i, j = 0, 0
+        for q in que:
+            t, idx = q
+            while j < ll and logs[j][1] <= t:
+                pre[logs[j][0]] += 1
+                j += 1
+            while i < ll and logs[i][1] < t - x:
+                pre[logs[i][0]] -= 1
+                if pre[logs[i][0]] == 0:
+                    del pre[logs[i][0]]
+                i += 1
+            res[idx] = n - len(pre)
+        return res
+
 
 
