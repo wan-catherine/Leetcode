4c2742afd9f97ea155b61894049304bf3c57c07a
wan.catherine <rarry1988528@126.com>
2025-03-28 15:05:06 -0400

    2503

--- problems/N2503_Maximum_Number_Of_Points_From_Grid_Queries.py
+++ problems/N2503_Maximum_Number_Of_Points_From_Grid_Queries.py
@@ -25,3 +25,41 @@ class Solution:
                     heapq.heappush(pq, (grid[row][col], row, col))
             res[q] = len(visited) - len(pq)
         return [res[q] for q in queries]
+
+    def maxPoints_20250328(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        lq = len(queries)
+        que = [(queries[i], i) for i in range(lq)]
+        que.sort(key=lambda x: x[0])
+        res = [0] * lq
+        cur = 0
+        stack = [(0,0)]
+        visited = [[False] * cols for _ in range(rows)]
+        visited[0][0] = True
+        memo = {}
+        for v, idx in que:
+            if v in memo:
+                res[idx] = memo[v]
+                continue
+            left = []
+            while True:
+                nstack = []
+                for r, c in stack:
+                    if grid[r][c] >= v:
+                        left.append((r, c))
+                        continue
+                    cur += 1
+                    for i, j in directions:
+                        row, col = r + i, c + j
+                        if row < 0 or row >= rows or col < 0 or col >= cols or visited[row][col]:
+                            continue
+                        nstack.append((row, col))
+                        visited[row][col] = True
+                if not nstack:
+                    break
+                stack = nstack
+            stack = left
+            memo[v] = cur
+            res[idx] = cur
+        return res
